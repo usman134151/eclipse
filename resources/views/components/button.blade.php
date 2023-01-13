8c280add17cd0c11fd3c1abcb55fc7a95ffa16fa
Labib Muhammad Jamal <lj@ageogroup.co.uk>
2023-01-13 17:23:48 +0500

    brand colors added

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -2,7 +2,7 @@
 
 @php
   $variantClass = [
-      'primary' => 'text-white bg-indigo-500 hover:bg-indigo-600 focus:border-indigo-700 focus:bg-indigo-700 active:bg-indigo-700',
+      'primary' => 'bg-gradient-to-r from-brand-tertiary to-brand-secondary hover:bg-gradient-to-l hover:from-ep-blue hover:to-ep-blue text-white',
       'secondary' => 'bg-white text-red-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-red-500',
   ][$variant];
 @endphp
