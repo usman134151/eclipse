81c5d9b8a5be6d8a945b1e46354e7deef0df0faf
Labib Muhammad Jamal <lj@ageogroup.co.uk>
2023-01-13 21:14:08 +0500

    layout changes for tenants reset page

--- resources/views/tenant/auth/passwords/reset.blade.php
+++ resources/views/tenant/auth/passwords/reset.blade.php
@@ -1,6 +1,63 @@
 @extends('layouts.tenant')
 
 @section('content')
+<div class="w-8/12 flex flex-column justify-center items-center bg-[#f8f8f8]">
+      <div class="w-7/12">
+        <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+        <lottie-player src="https://abmasoft.dev-tenant.eclipsescheduling.com/assets/video/video.json" background="transparent" speed="1" classname="w-full" loop="" autoplay="true"></lottie-player>
+      </div>
+    </div>
+    <div class="w-4/12 flex flex-col justify-center px-14">
+      <h1>Welcome to Eclipse Scheduling!</h1>
+      <p class="mb-5">Please sign into your account to start exploring.</p>
+      <form class="w-full p-6" method="POST" action="{{ route('password.update') }}">
+                        @csrf
+
+                        <input type="hidden" name="token" value="{{ $token }}">
+
+                        <div class="flex flex-wrap mb-6">
+                            <label for="email" class="block text-gray-700 text-sm font-bold mb-2">
+                                {{ __('E-Mail Address') }}:
+                            </label>
+
+                            <input id="email" type="email" class="form-input w-full @error('email') border-red-500 @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+
+                            @error('email')
+                                <p class="text-red-500 text-xs italic mt-4">
+                                    {{ $message }}
+                                </p>
+                            @enderror
+                        </div>
+
+                        <div class="flex flex-wrap mb-6">
+                            <label for="password" class="block text-gray-700 text-sm font-bold mb-2">
+                                {{ __('Password') }}:
+                            </label>
+
+                            <input id="password" type="password" class="form-input w-full @error('password') border-red-500 @enderror" name="password" required autocomplete="new-password">
+
+                            @error('password')
+                                <p class="text-red-500 text-xs italic mt-4">
+                                    {{ $message }}
+                                </p>
+                            @enderror
+                        </div>
+
+                        <div class="flex flex-wrap mb-6">
+                            <label for="password-confirm" class="block text-gray-700 text-sm font-bold mb-2">
+                                {{ __('Confirm Password') }}:
+                            </label>
+
+                            <input id="password-confirm" type="password" class="form-input w-full" name="password_confirmation" required autocomplete="new-password">
+                        </div>
+
+                        <div class="flex flex-wrap">
+                            <x-button class="w-full" type="submit">Reset Password</x-button>
+                        </div>
+                    </form>
+      </div>
+
+    <?php /*
     <div class="container mx-auto">
         <div class="flex flex-wrap justify-center">
             <div class="w-full max-w-sm">
@@ -60,4 +117,5 @@
             </div>
         </div>
     </div>
+    */ ?>
 @endsection
