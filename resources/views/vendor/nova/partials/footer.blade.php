af4c6117f6e438848aebde516eb6fd2222bfb8ad
Labib Muhammad Jamal <lj@ageogroup.co.uk>
2023-01-13 22:55:20 +0500

    footer updated for tenants sidebar logged in state

--- resources/views/vendor/nova/partials/footer.blade.php
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -1,3 +1,4 @@
+<?php /*
 <p class="mt-8 text-center text-xs text-80">
     <a href="https://nova.laravel.com" class="text-primary dim no-underline">Laravel Nova</a>
     <span class="px-1">&middot;</span>
@@ -5,3 +6,4 @@
     <span class="px-1">&middot;</span>
     v{{ \Laravel\Nova\Nova::version() }}
 </p>
+*/ ?>
