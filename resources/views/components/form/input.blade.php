e09e30891d2dc8df1557239b846d13f158309200
Labib Muhammad Jamal <lj@ageogroup.co.uk>
2023-01-17 02:45:57 +0500

    form elements

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,4 +1,4 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => "border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 placeholder-gray-400 rounded-md shadow-sm mt-1 block w-full"]) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => "placeholder-gray-500 leading-none text-gray-800 rounded w-full py-2 px-2 border border-gray-600 active:border-[#213969] transition ease-in-out focus:outline-none focus:border-[#023DB0] focus:ring-0 mt-1 block w-full"]) !!}>
 
