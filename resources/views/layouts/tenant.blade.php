d1abbc12c21b5595123274579ddf92b95286bb1b
Labib Muhammad Jamal <lj@ageogroup.co.uk>
2023-01-16 19:06:29 +0500

    application menu updates

--- resources/views/layouts/tenant.blade.php
+++ resources/views/layouts/tenant.blade.php
@@ -27,7 +27,7 @@
         </div>
         <div class="w-8/12 ml-auto">
             
-        </div>
+        
         <nav class="bg-gray-900">
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div class="flex items-center justify-between h-16">
