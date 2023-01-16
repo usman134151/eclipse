9f0135f78ceaaba669a261dbf7d39096ee81153a
Labib Muhammad Jamal <lj@ageogroup.co.uk>
2023-01-17 02:56:39 +0500

    form elements

--- resources/views/tenant/posts/create.blade.php
+++ resources/views/tenant/posts/create.blade.php
@@ -8,19 +8,34 @@
         <div>
             <div class="grid grid-cols-1 sm:grid-cols-6 gap-6">
                 <div class="sm:col-span-3">
-                    <x-form.label for="title" value="Title" class="label-text"/>
+                    <x-form.label for="title" value="Title"/>
 
                     <div class="mt-1 flex rounded-md shadow-sm">
-                        <x-form.input id="title" name="title" type="text" value="{{ old('title') }}" class="input-field" />
+                        <x-form.input id="title" name="title" type="text" value="{{ old('title') }}" />
                     </div>
 
                     <x-form.input-error for="title" />
                 </div>
+                <?php /*
+                <div class="sm:col-span-3">
+                    <x-form.label for="title" value="Title"/>
+
+                    <div class="mt-1 flex rounded-md shadow-sm">
+                        <x-form.select>
+                            <option>A</option>
+                            <option>B</option>
+                            <option>C</option>
+                        </x-form.select>
+                    </div>
+
+                    <x-form.input-error for="title" />
+                </div>
+                */ ?>
 
                 <div class="sm:col-span-6">
                     <x-form.label for="body" value="Body"/>
                     <div class="mt-1 rounded-md shadow-sm">
-                        <textarea id="body" name="body" rows="5" class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 placeholder-gray-400 rounded-md shadow-sm mt-1 block w-full @error('body') border-red-500 @enderror">{{ old('body') }}</textarea>
+                        <x-form-textarea id="body" name="body" rows="5">{{ old('body') }}</x-form-textarea>
                     </div>
                     <x-form.input-error for="body" />
                 </div>
