5b322b542abaadeef9338b74ab71afacb2210dc9
Labib Muhammad Jamal <lj@ageogroup.co.uk>
2023-01-16 23:01:56 +0500

    application menu updates

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -134,7 +134,7 @@
         <ul class="menu-content " id="Bookings" x-ref="panel"
             x-show="open"
             x-transition.origin.top.left
-            x-on:click.outside="open = true"
+            x-on:click.outside="close($refs.button)"
             :id="$id('dropdown-button')"
             style="display: none;">
           <li class="nav-item ">
