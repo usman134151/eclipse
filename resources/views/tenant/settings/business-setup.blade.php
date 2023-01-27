68c5ca60b5e794d4b788cb9dce64ce460a14d35a
Sohail Asghar <sohail@abmasoft.com>
2023-01-27 09:46:37 +0500

    Added route for business setup

---
+++ resources/views/tenant/settings/business-setup.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.tenant', ['title' => 'Setting'])
+
+@section('content')
+	{{-- BEGIN: Content --}}
+	<div class="app-content content">
+        Add <b>Livewire</b> component here
+		{{-- @livewire('app.common.forms.change-password', ['showForm'=>$showForm]) --}}
+	</div>
+	{{-- End: Content --}}
+@endsection
