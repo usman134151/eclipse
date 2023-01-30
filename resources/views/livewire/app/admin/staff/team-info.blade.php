2b48e0efb1bd941eb4fffd60b53e0f2cb7a25843
Sohail Asghar <sohail@abmasoft.com>
2023-01-30 17:45:15 +0000

    Added Admin Staff Livewire structure

---
+++ resources/views/livewire/app/admin/staff/team-info.blade.php
@@ -0,0 +1,124 @@
+<div>
+	<form class="form">
+		<div class="row mt-2 mb-5">
+		  <div class="col-12 text-center">
+			<div class="d-inline-block position-relative">
+			  <img src="/tenant/images/portrait/small/avatar-s-9.jpg" class="img-fluid rounded-circle" alt="Profile Image of Admin Staff Team"/>
+			  <!-- <div>
+				<input class="position-absolute form-control" type="file" />
+			  </div> -->
+			  <div class="position-absolute end-0 bottom-0 p-0 d-flex justify-content-center align-items-center">
+				<svg width="57" height="57" viewBox="0 0 57 57" fill="none" xmlns="http://www.w3.org/2000/svg">
+				  <circle cx="28.5" cy="28.5" r="28" fill="#0A1E46" stroke="white"/>
+				  <path d="M42.9375 37.625C42.9375 38.172 42.7202 38.6966 42.3334 39.0834C41.9466 39.4702 41.422 39.6875 40.875 39.6875H16.125C15.578 39.6875 15.0534 39.4702 14.6666 39.0834C14.2798 38.6966 14.0625 38.172 14.0625 37.625V25.25C14.0625 24.703 14.2798 24.1784 14.6666 23.7916C15.0534 23.4048 15.578 23.1875 16.125 23.1875H18.5422C20.1824 23.1866 21.7551 22.5345 22.9147 21.3746L24.6266 19.6668C25.0123 19.281 25.5352 19.0637 26.0807 19.0625H30.9152C31.4622 19.0626 31.9867 19.28 32.3734 19.6668L34.0811 21.3746C34.6558 21.9494 35.3381 22.4054 36.0891 22.7165C36.84 23.0275 37.6449 23.1876 38.4578 23.1875H40.875C41.422 23.1875 41.9466 23.4048 42.3334 23.7916C42.7202 24.1784 42.9375 24.703 42.9375 25.25V37.625ZM16.125 21.125C15.031 21.125 13.9818 21.5596 13.2082 22.3332C12.4346 23.1068 12 24.156 12 25.25V37.625C12 38.719 12.4346 39.7682 13.2082 40.5418C13.9818 41.3154 15.031 41.75 16.125 41.75H40.875C41.969 41.75 43.0182 41.3154 43.7918 40.5418C44.5654 39.7682 45 38.719 45 37.625V25.25C45 24.156 44.5654 23.1068 43.7918 22.3332C43.0182 21.5596 41.969 21.125 40.875 21.125H38.4578C37.3638 21.1248 36.3148 20.69 35.5414 19.9164L33.8336 18.2086C33.0602 17.435 32.0112 17.0002 30.9172 17H26.0828C24.9888 17.0002 23.9398 17.435 23.1664 18.2086L21.4586 19.9164C20.6852 20.69 19.6362 21.1248 18.5422 21.125H16.125Z" fill="white"/>
+				  <path d="M28.5 35.5625C27.1325 35.5625 25.821 35.0193 24.854 34.0523C23.887 33.0853 23.3438 31.7738 23.3438 30.4063C23.3438 29.0387 23.887 27.7272 24.854 26.7602C25.821 25.7932 27.1325 25.25 28.5 25.25C29.8675 25.25 31.179 25.7932 32.146 26.7602C33.113 27.7272 33.6562 29.0387 33.6562 30.4063C33.6562 31.7738 33.113 33.0853 32.146 34.0523C31.179 35.0193 29.8675 35.5625 28.5 35.5625ZM28.5 37.625C30.4145 37.625 32.2506 36.8645 33.6044 35.5107C34.9582 34.1569 35.7188 32.3208 35.7188 30.4063C35.7188 28.4917 34.9582 26.6556 33.6044 25.3018C32.2506 23.948 30.4145 23.1875 28.5 23.1875C26.5855 23.1875 24.7494 23.948 23.3956 25.3018C22.0418 26.6556 21.2812 28.4917 21.2812 30.4063C21.2812 32.3208 22.0418 34.1569 23.3956 35.5107C24.7494 36.8645 26.5855 37.625 28.5 37.625ZM18.1875 26.2813C18.1875 26.5548 18.0789 26.8171 17.8855 27.0105C17.6921 27.2039 17.4298 27.3125 17.1562 27.3125C16.8827 27.3125 16.6204 27.2039 16.427 27.0105C16.2336 26.8171 16.125 26.5548 16.125 26.2813C16.125 26.0077 16.2336 25.7454 16.427 25.552C16.6204 25.3586 16.8827 25.25 17.1562 25.25C17.4298 25.25 17.6921 25.3586 17.8855 25.552C18.0789 25.7454 18.1875 26.0077 18.1875 26.2813Z" fill="white"/>
+				  </svg>
+			  </div>
+			</div>
+		  </div>
+		</div>
+		<div class="row">
+		  <div class="col-md-12 mb-md-2">
+			<h2 class="mb-5">Team Info</h2>
+			<div class="row">
+			  <div class="col-md-6 col-12">
+				<div class="mb-4">
+				  <label class="form-label" for="team_name">
+					Team Name <span class="mandatory" aria-hidden="true">*</span>
+				  </label>
+				  <input
+					type="text"
+					id="team_name"
+					class="form-control"
+					name="team_name"
+					placeholder="Enter First Name"
+					required
+					aria-required="true"
+					/>
+				</div>
+			  </div>
+			  <div class="col-md-6 col-12">
+				<div class="mb-4">
+				  <label class="form-label">
+					Lead Admin(s) <span class="mandatory" aria-hidden="true">*</span>
+				  </label>
+				  <select class="select2 form-select" aria-label="Select Lead Admin" name="lead-admin" required aria-required="true">
+					<option>Enter Lead Admin</option>
+					<option>Wade Dave</option>
+					<option>Dori Griffiths</option>
+					<option>Gilbert Dan</option>
+					<option>Ramon Miles</option>
+					<option>Alexis Griffiths</option>
+					<option>Tessa Leo</option>
+					<option>John Cris</option>
+				  </select>
+				</div>
+			  </div>
+			  <div class="col-md-6 col-12">
+				<div class="mb-4">
+				  <label class="form-label" for="team_lead_email">
+					Team Lead Email
+				  </label>
+				  <input
+					type="email"
+					id="team_lead_email"
+					name="team_lead_email"
+					class="form-control"
+					placeholder="Enter Team Lead Email"
+					/>
+				</div>
+			  </div>
+			  <div class="col-md-6 col-12">
+				<div class="mb-4">
+				  <label class="form-label" for="team_lead_phone_number">
+					Team Lead Phone Number
+				  </label>
+				  <input
+					type="text"
+					id="team_lead_phone_number"
+					class="form-control"
+					placeholder="Enter Team Lead Phone Number"
+					name="team_lead_phone_number"
+					/>
+				</div>
+			  </div>
+			  <div class="col-md-6 col-12">
+				<div class="mb-4">
+				  <label class="form-label" for="team_description">
+					Team Description
+				  </label>
+				  <textarea
+				  class="form-control"
+				  placeholder="Add Team Description"
+				  name="team_description"
+				  id="team_description"
+				  ></textarea>
+				</div>
+			  </div>
+			  <div class="col-md-6 col-12">
+				<div class="mb-4">
+				  <label class="form-label" for="team_notes">
+					Team Notes
+				  </label>
+				  <textarea
+				  class="form-control"
+				  placeholder="Add Team Notes"
+				  name="team_notes"
+				  id="team_notes"
+				  ></textarea>
+				</div>
+			  </div>
+			</div>
+		  </div>
+		  <div class="d-flex justify-content-center gap-2 col-12 mt-5 form-actions">
+			<a
+				href="javascript:void(0);"
+				class="btn btn-secondary rounded px-4"
+				role="button"
+			>
+			Back</a>
+			<button type="submit" class="btn btn-primary rounded px-4">Next</button>
+		  </div>
+		</div>
+	  </form>
+</div>
