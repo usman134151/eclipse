7cb20b2572ef0414ced26eb669f02e2745feaa00
Labib Jamal <labib@abmasoft.com>
2023-01-30 16:56:54 +0000

    Dev labib

--- app/Http/Livewire/App/Common/Customer.php
+++ app/Http/Livewire/App/Common/Customer.php
@@ -6,6 +6,31 @@ use Livewire\Component;
 
 class Customer extends Component
 {
+<<<<<<< HEAD
+    public $showForm;
+    protected $listeners = ['showList' => 'resetForm'];
+
+    public function render()
+    {
+        return view('livewire.app.common.customer');
+    }
+
+    public function mount()
+    {
+       
+       
+    }
+
+    function showForm()
+    {     
+       $this->showForm=true;
+    }
+    public function resetForm()
+    {
+        $this->showForm=false;
+    }
+
+=======
 	public $showForm;
 	protected $listeners = ['showList'=>'resetForm'];
 
@@ -25,4 +50,5 @@ class Customer extends Component
 	{
 		return view('livewire.app.common.customer');
 	}
+>>>>>>> bb8370cea39f57f5d9ccae6af6aa8c31705d9707
 }
