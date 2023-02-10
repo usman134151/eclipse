fe46a2b8138547d578459627ece1b454c1fb8f9c
Sohail Asghar <sohail@abmasoft.com>
2023-02-10 16:54:19 +0000

    Fix on user notifications missing view

--- resources/views/tenant/common/notifications.blade.php
+++ resources/views/tenant/common/notifications.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 	{{-- BEGIN: Content --}}
-	@livewire('app.common.notifications', ['showForm'=>$showForm])
+	{{-- @livewire('app.common.notifications', ['showForm'=>$showForm]) --}}
+	<x-coming-soon></x-coming-soon>
 	{{-- End: Content --}}
 @endsection
