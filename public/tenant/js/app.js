1507ef3c68720877123d07247bb3457944a6711b
Labib Muhammad Jamal <lj@ageogroup.co.uk>
2023-03-07 19:42:41 +0500

    8- Please confirm if this tooltip style is done -> Style added -> issue remittance blade template view

--- public/tenant/js/app.js
+++ public/tenant/js/app.js
@@ -197,3 +197,9 @@ $('.js-selected-row').click(function(){
     $('.js-tick').removeClass("d-none");
     $(this).siblings().removeClass("selected");
 });
+
+// Initialize Tooltip
+
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
