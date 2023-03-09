0e736066ed1862b2260e800d65198fce7989000a
Amna Bilal <amnausman@gmail.com>
2023-03-09 19:54:25 +0500

    SVG Sprite for powergrid

--- app/Http/Livewire/App/Common/Specializationmain.php
+++ app/Http/Livewire/App/Common/Specializationmain.php
@@ -7,55 +7,70 @@ use App\Models\Tenant\Specialization;
 
 class Specializationmain extends Component
 {
+    // properties for displaying/hiding the form and displaying confirmation message
     public $showForm;
     public $confirmationMessage;
+    // property for holding the ID of the record being edited/deleted
     public $recordId;
-    protected $listeners = ['showList' => 'resetForm','showForm'=>'showForm','delete'=>'deleteRecord','updateRecordId'=>'updateRecordId'];
+    // define event listeners for this component
+    protected $listeners = [
+        'showList' => 'resetForm', // reset form when the parent component shows a list
+        'showForm' => 'showForm', // show form when the parent component requests it
+        'delete' => 'deleteRecord', // delete the record with the specified ID
+        'updateRecordId' => 'updateRecordId', // update the ID of the record being edited/deleted
+    ];
 
     public function mount()
     {
-       
-       
+        // This function runs when the component is mounted
+        // No code here at the moment
     }
 
-    function showForm($specialization=null){
-
-        if($specialization){
-            $this->specialization=$specialization;
-            $this->emit('editRecord',$specialization);
+    // function to show the form
+    function showForm($specialization = null)
+    {
+        if ($specialization) {
+            $this->specialization = $specialization;
+            $this->emit('editRecord', $specialization);
         }
-        
-        $this->showForm=true;
+        // set the showForm property to true to display the form
+        $this->showForm = true;
     }
+
+    // function to reset the form and display a confirmation message
     public function resetForm($message)
     {
-        if($message){
-            $this->confirmationMessage=$message;
-       
+        if ($message) {
+            $this->confirmationMessage = $message;
+            // emit an event to display a success message using the SweetAlert package
             $this->dispatchBrowserEvent('swal:modal', [
                 'type' => 'success',
                 'title' => 'Success',
                 'text' => $message,
             ]);
-
-            
-            
         }
+        // set the showForm property to false to hide the form
+        $this->showForm = false;
+    }
 
-        $this->showForm=false;
+    // function to update the ID of the record being edited/deleted
+    public function updateRecordId($id)
+    {
+        $this->recordId = $id;
     }
-    public function updateRecordId($id){
-        $this->recordId=$id;
+
+    // function to delete the record with the specified ID
+    public function deleteRecord()
+    {
+        // delete the record from the database using the Specialization model
+        Specialization::where('id', $this->recordId)->delete();
+        // emit an event to reset the form and display a confirmation message
+        $this->emitSelf('showList', 'Record has been deleted');
     }
-    public function deleteRecord(){
-       
-        Specialization::where('id',$this->recordId)->delete();
-        $this->emitSelf('showList','Record has been deleted');
-              
-    }
+
     public function render()
     {
-       
+        // render the component view
         return view('livewire.app.common.specialization');
     }
 }
