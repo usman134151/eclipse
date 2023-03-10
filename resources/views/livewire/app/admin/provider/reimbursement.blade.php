d806e705d0ae2331d28cc061ae3c127899732512
Labib Jamal <labib@abmasoft.com>
2023-03-10 17:58:19 +0000

    payment manager -> small/iPad screen -> fixes + sidebar menu links left space reduces -> small screen + mobile/iPad responsive updated -> business setup screen

--- resources/views/livewire/app/admin/provider/reimbursement.blade.php
+++ resources/views/livewire/app/admin/provider/reimbursement.blade.php
@@ -48,74 +48,68 @@
 						<p>Here you can manage your providers' reimbursements for each assignment they work. Once approved, reimbursements will appear in the provider's
 payroll in "Remittances."</p>
                        <div class="col-12">
-						<div class="row gap-3 bg-light p-3 mb-2">
-						<div class="col-3">
-							<label for="search" class="form-label fw-semibold">Search</label>
-							<div class="d-flex align-items-center gap-2">
-								<input type="search" class="form-control" id="search" name="search" placeholder="Provider Name or Email" autocomplete="on"/>
-								<svg width="40" height="40" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle cx="15" cy="15" r="15" transform="rotate(90 15 15)" fill="url(#paint0_linear_0_1)"/>
-<circle cx="15" cy="15" r="15" transform="rotate(90 15 15)" fill="url(#paint1_linear_0_1)"/>
-<path d="M22.023 19.977C21.5546 19.5284 21.0988 19.0669 20.656 18.593C20.284 18.215 20.06 17.94 20.06 17.94L17.26 16.603C18.381 15.3316 18.9997 13.695 19 12C19 8.141 15.86 5 12 5C8.14 5 5 8.141 5 12C5 15.859 8.14 19 12 19C13.763 19 15.37 18.34 16.603 17.261L17.94 20.061C17.94 20.061 18.215 20.285 18.593 20.657C18.98 21.02 19.489 21.511 19.977 22.024L21.335 23.416L21.939 24.062L24.06 21.941L23.414 21.337C23.035 20.965 22.529 20.471 22.023 19.977V19.977ZM12 17C9.243 17 7 14.757 7 12C7 9.243 9.243 7 12 7C14.757 7 17 9.243 17 12C17 14.757 14.757 17 12 17Z" fill="black"/>
-<defs>
-<linearGradient id="paint0_linear_0_1" x1="-9.53674e-07" y1="-0.154101" x2="-9.53674e-07" y2="30" gradientUnits="userSpaceOnUse">
-<stop offset="0.0150376" stop-color="#C4C4C4"/>
-<stop offset="1" stop-color="white" stop-opacity="0.01"/>
-</linearGradient>
-<linearGradient id="paint1_linear_0_1" x1="-9.53674e-07" y1="-0.154101" x2="-9.53674e-07" y2="30" gradientUnits="userSpaceOnUse">
-<stop offset="0.0114183" stop-color="white" stop-opacity="0.01"/>
-<stop offset="1" stop-opacity="0.01"/>
-</linearGradient>
-</defs>
-</svg>
-
-							</div>
-
-
-							</div>
-							<div class="col-3">
-                            <label class="form-label fw-semibold">Data Range</label>
-                            <div class="d-grid grid-cols-3 gap-4">
-                              <div class="form-check">
-                                <input class="form-check-input" id="booking" name="dateRangeGroup" type="radio" tabindex="" />
-                                <label class="form-check-label" for="booking">Booking</label>
-                              </div>
-                              <div class="form-check">
-                                <input class="form-check-input" id="issued" name="dateRangeGroup" type="radio" tabindex="" />
-                                <label class="form-check-label" for="issued"> Issued</label>
-                              </div>
-                              <div class="form-check">
-                                <input class="form-check-input" id="paid" name="dateRangeGroup" type="radio" tabindex="" />
-                                <label class="form-check-label" for="paid">Paid</label>
-                              </div>
+						
+						<!-- BEGIN: Filters -->
+                  <div class="bg-muted rounded p-4 mb-1">
+                    <div class="d-lg-flex gap-5 align-items-center">
+                      <div class="mb-4 mb-lg-0">
+                        <label class="form-label-sm">Search</label>
+                        <div class="d-flex gap-2 align-items-center">
+                          <div class="position-relative">
+                            <input type="text" class="form-control form-control-md is-search" id="search" aria-describedby="search" placeholder="Provider Name or Email">
+                            <x-icon name="cancel"/>
+                          </div>
+                          <button class="btn btn-secondary rounded btn-sm btn-hs-icon">
+                            <x-icon name="search"/>
+                          </button>
+                        </div>
+                      </div>
+                      <div class="mb-4 mb-lg-0">
+                        <label class="form-label-sm">Date Range</label>
+                        <div class="d-md-flex gap-2" >    
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="dateRange" id="booking">
+                                <label class="form-check-label-sm" for="booking">
+                                    Booking
+                                </label>
                             </div>
-							<div class="d-inline-flex align-items-center gap-4">
-        <!-- Begin : it will be replaced with livewire module-->
-        <input type="" class="form-control w-auto js-single-date" placeholder="MM/DD/YYYY" name="selectDate" aria-label="Select Date">
-        <!-- End : it will be replaced with livewire module -->
-      </div>
-							</div>
-							<div class="col-4">
-								<div class="d-flex gap-4">
-									<div>
-									<label for="review-status" class="form-label fw-semibold">
-										Review Status
-									</label>
-									<select class="form-select" id="review-status">
-										<option>Pending</option>
-									</select>
-									</div>
-									<div>
-									<label for="payment-status" class="form-label fw-semibold">
-										Payment Status
-									</label>
-									<select class="form-select" id="payment-status">
-										<option>Pending</option>
-									</select>
-									</div>
-								</div>
-							</div>
-						</div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="dateRange" id="issued">
+                                <label class="form-check-label-sm" for="issued">
+                                    Issued
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="dateRange" id="piad">
+                                <label class="form-check-label-sm" for="piad">
+                                    Piad
+                                </label>
+                            </div>
+                        </div>
+                        <div class="mb-4 mb-lg-0 position-relative has-date-icon-left-side">
+                          <x-icon name="input-calender"/>
+                          <input type="" class="form-control form-control-md js-single-date" placeholder="MM/DD/YYYY" name="selectDate" aria-label="Select Date">
+                        </div>
+                      </div>
+                      <div class="mb-4 mb-lg-0">
+							<label class="form-label-sm" for="review-status-column">
+								Review Status
+							</label>
+							<select class="select2 form-select form-select-md" id="review-status-column">
+								<option>Pending</option>
+							</select>
+                      </div>
+                      <div class="mb-4 mb-lg-0">
+							<label class="form-label-sm" for="payment-status-column">
+								Payment Status
+							</label>
+							<select class="select2 form-select form-select-md" id="payment-status-column">
+								<option>Pending</option>
+							</select>
+                      </div>
+                    </div>
+                  </div>
+                  <!-- END: Filters -->
 						<div class="mb-4">
 								<input class="form-check-input" type="checkbox" value="select-all-providers" id="select-all-providers">
 								<label class="form-check-label fw-semibold" for="select-all-providers">
@@ -125,7 +119,7 @@ payroll in "Remittances."</p>
 							<div class="row">
 								<div class="dropdown">
 
-                              <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                              <button class="btn btn-outline-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 <svg width="23" height="26" viewBox="0 0 23 26" fill="none" xmlns="http://www.w3.org/2000/svg">
                                   <path d="M10 0.5V8.625C10 9.12228 10.1975 9.5992 10.5492 9.95083C10.9008 10.3025 11.3777 10.5 11.875 10.5H20V17.6963L18.3837 16.08C18.2676 15.9639 18.1298 15.8719 17.978 15.8091C17.8263 15.7463 17.6637 15.7141 17.4996 15.7141C17.3354 15.7142 17.1728 15.7466 17.0211 15.8095C16.8695 15.8723 16.7317 15.9645 16.6156 16.0806C16.4996 16.1968 16.4075 16.3346 16.3447 16.4863C16.282 16.638 16.2497 16.8006 16.2497 16.9648C16.2498 17.129 16.2822 17.2916 16.3451 17.4432C16.408 17.5949 16.5001 17.7327 16.6162 17.8488L18.0175 19.25H12.5C12.1685 19.25 11.8505 19.3817 11.6161 19.6161C11.3817 19.8505 11.25 20.1685 11.25 20.5C11.25 20.8315 11.3817 21.1495 11.6161 21.3839C11.8505 21.6183 12.1685 21.75 12.5 21.75H18.0175L16.6162 23.1513C16.3817 23.3856 16.2499 23.7036 16.2497 24.0352C16.2496 24.3668 16.3812 24.6848 16.6156 24.9194C16.85 25.1539 17.168 25.2858 17.4996 25.2859C17.8311 25.286 18.1492 25.1544 18.3837 24.92L19.9787 23.3238C19.9002 23.9256 19.6053 24.4783 19.1492 24.8787C18.6931 25.2791 18.1069 25.5 17.5 25.5H2.5C1.83696 25.5 1.20107 25.2366 0.732233 24.7678C0.263392 24.2989 0 23.663 0 23V3C0 2.33696 0.263392 1.70107 0.732233 1.23223C1.20107 0.763392 1.83696 0.5 2.5 0.5H10ZM20 17.6963L21.9187 19.6163C22.1531 19.8507 22.2847 20.1685 22.2847 20.5C22.2847 20.8315 22.1531 21.1493 21.9187 21.3838L20 23.3038V17.6963ZM12.5 0.55375C12.9736 0.654194 13.4078 0.889989 13.75 1.2325L19.2675 6.75C19.61 7.09216 19.8458 7.5264 19.9462 8H12.5V0.55375Z" fill="#023DB0"/>
                                   </svg>
@@ -147,7 +141,7 @@ payroll in "Remittances."</p>
 					</div>
 					</div>
 					</div>
-					<div class="d-flex justify-content-between mb-2">
+					<div class="d-flex flex-column flex-md-row justify-content-between mb-2 gap-2">
 					  <div class="d-inline-flex align-items-center gap-4">
 						<label for="show_records_number" class="form-label">Show</label>
 						<select class="form-select" id="show_records_number">
@@ -386,12 +380,12 @@ payroll in "Remittances."</p>
 							</table>
 						</div>
 
-				  <div class="d-flex justify-content-between">
+				  <div class="d-flex flex-column flex-md-row justify-content-between">
 					<div>
 					  <p class="fw-semibold">Showing 1 to 5 of 30 entries</p>
 					</div>
 					<nav aria-label="Page Navigation">
-					  <ul class="pagination">
+					  <ul class="pagination justify-content-start justify-content-lg-end">
 						<li class="page-item">
 						  <a class="page-link" href="#" aria-label="Previous">Previous
 							<span aria-hidden="true">&laquo;</span>
@@ -451,13 +445,9 @@ payroll in "Remittances."</p>
                                         </label>
                                      </div>
                                     </div>
-									<div class="d-flex justify-content-center gap-2">
-									 <div class="mb-4">
+									<div class="d-flex form-actions flex-lg-row flex-column justify-content-center gap-2">
 									 <a href="#" class="btn btn-primary rounded">Approve Reimbursement</a>
-                                     </div>
-									 <div class="mb-4">
 									 <a href="#" class="btn btn-primary rounded">Decline Reimburement</a>
-                                     </div>
                                     </div>
                                  </div>
 			</div>
