b129d320634d0c8e7fa0c7b0673f30f316a90b6e
Labib Muhammad Jamal <lj@ageogroup.co.uk>
2023-03-13 18:00:40 +0500

    Add Reimbursement button fixes -> iPad/mobile screen

--- resources/views/livewire/app/admin/provider/reimbursement.blade.php
+++ resources/views/livewire/app/admin/provider/reimbursement.blade.php
@@ -43,103 +43,104 @@
 			<div class="card">
 			  <div class="card-body">
 				  <div class="row">
-					<div class="col-md-12 mb-md-2">
-					  <div class="row">
-						<p>Here you can manage your providers' reimbursements for each assignment they work. Once approved, reimbursements will appear in the provider's
-payroll in "Remittances."</p>
-                       <div class="col-12">
-						
-						<!-- BEGIN: Filters -->
-                  <div class="bg-muted rounded p-4 mb-1">
-                    <div class="d-lg-flex gap-5 align-items-center">
-                      <div class="mb-4 mb-lg-0">
-                        <label class="form-label-sm">Search</label>
-                        <div class="d-flex gap-2 align-items-center">
-                          <div class="position-relative">
-                            <input type="text" class="form-control form-control-md is-search" id="search" aria-describedby="search" placeholder="Provider Name or Email">
-                            <x-icon name="cancel"/>
-                          </div>
-                          <button class="btn btn-secondary rounded btn-sm btn-hs-icon">
-                            <x-icon name="search"/>
-                          </button>
-                        </div>
-                      </div>
-                      <div class="mb-4 mb-lg-0">
-                        <label class="form-label-sm">Date Range</label>
-                        <div class="d-md-flex gap-2" >    
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="dateRange" id="booking">
-                                <label class="form-check-label-sm" for="booking">
-                                    Booking
-                                </label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="dateRange" id="issued">
-                                <label class="form-check-label-sm" for="issued">
-                                    Issued
-                                </label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="dateRange" id="piad">
-                                <label class="form-check-label-sm" for="piad">
-                                    Piad
-                                </label>
-                            </div>
-                        </div>
-                        <div class="mb-4 mb-lg-0 position-relative has-date-icon-left-side">
-                          <x-icon name="input-calender"/>
-                          <input type="" class="form-control form-control-md js-single-date" placeholder="MM/DD/YYYY" name="selectDate" aria-label="Select Date">
-                        </div>
-                      </div>
-                      <div class="mb-4 mb-lg-0">
-							<label class="form-label-sm" for="review-status-column">
-								Review Status
-							</label>
-							<select class="select2 form-select form-select-md" id="review-status-column">
-								<option>Pending</option>
-							</select>
-                      </div>
-                      <div class="mb-4 mb-lg-0">
-							<label class="form-label-sm" for="payment-status-column">
-								Payment Status
-							</label>
-							<select class="select2 form-select form-select-md" id="payment-status-column">
-								<option>Pending</option>
-							</select>
-                      </div>
-                    </div>
-                  </div>
-                  <!-- END: Filters -->
-						<div class="mb-4">
-								<input class="form-check-input" type="checkbox" value="select-all-providers" id="select-all-providers">
-								<label class="form-check-label fw-semibold" for="select-all-providers">
-									Select All Providers
-								</label>
-							</div>
-							<div class="row">
-								<div class="dropdown">
-
-                              <button class="btn btn-outline-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <svg width="23" height="26" viewBox="0 0 23 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                  <path d="M10 0.5V8.625C10 9.12228 10.1975 9.5992 10.5492 9.95083C10.9008 10.3025 11.3777 10.5 11.875 10.5H20V17.6963L18.3837 16.08C18.2676 15.9639 18.1298 15.8719 17.978 15.8091C17.8263 15.7463 17.6637 15.7141 17.4996 15.7141C17.3354 15.7142 17.1728 15.7466 17.0211 15.8095C16.8695 15.8723 16.7317 15.9645 16.6156 16.0806C16.4996 16.1968 16.4075 16.3346 16.3447 16.4863C16.282 16.638 16.2497 16.8006 16.2497 16.9648C16.2498 17.129 16.2822 17.2916 16.3451 17.4432C16.408 17.5949 16.5001 17.7327 16.6162 17.8488L18.0175 19.25H12.5C12.1685 19.25 11.8505 19.3817 11.6161 19.6161C11.3817 19.8505 11.25 20.1685 11.25 20.5C11.25 20.8315 11.3817 21.1495 11.6161 21.3839C11.8505 21.6183 12.1685 21.75 12.5 21.75H18.0175L16.6162 23.1513C16.3817 23.3856 16.2499 23.7036 16.2497 24.0352C16.2496 24.3668 16.3812 24.6848 16.6156 24.9194C16.85 25.1539 17.168 25.2858 17.4996 25.2859C17.8311 25.286 18.1492 25.1544 18.3837 24.92L19.9787 23.3238C19.9002 23.9256 19.6053 24.4783 19.1492 24.8787C18.6931 25.2791 18.1069 25.5 17.5 25.5H2.5C1.83696 25.5 1.20107 25.2366 0.732233 24.7678C0.263392 24.2989 0 23.663 0 23V3C0 2.33696 0.263392 1.70107 0.732233 1.23223C1.20107 0.763392 1.83696 0.5 2.5 0.5H10ZM20 17.6963L21.9187 19.6163C22.1531 19.8507 22.2847 20.1685 22.2847 20.5C22.2847 20.8315 22.1531 21.1493 21.9187 21.3838L20 23.3038V17.6963ZM12.5 0.55375C12.9736 0.654194 13.4078 0.889989 13.75 1.2325L19.2675 6.75C19.61 7.09216 19.8458 7.5264 19.9462 8H12.5V0.55375Z" fill="#023DB0"/>
-                                  </svg>
-                              </button>
-                              <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                              </ul>
-                            </div>
-                          </div>
+						<div class="col-md-12 mb-md-2">
+						  <div class="row">
+	           		<div class="col-12">
+									<p>Here you can manage your providers' reimbursements for each assignment they work. Once approved, reimbursements will appear in the provider's payroll in "Remittances."</p>
+									<!-- BEGIN: Filters -->
+		              <div class="bg-muted rounded p-4 mb-1">
+		                <div class="d-lg-flex gap-5 align-items-center">
+		                  <div class="mb-4 mb-lg-0">
+		                    <label class="form-label-sm">Search</label>
+		                    <div class="d-flex gap-2 align-items-center">
+		                      <div class="position-relative">
+		                        <input type="text" class="form-control form-control-md is-search" id="search" aria-describedby="search" placeholder="Provider Name or Email">
+		                        <x-icon name="cancel"/>
+		                      </div>
+		                      <button class="btn btn-secondary rounded btn-sm btn-hs-icon">
+		                        <x-icon name="search"/>
+		                      </button>
+		                    </div>
+		                  </div>
+		                  <div class="mb-4 mb-lg-0">
+		                    <label class="form-label-sm">Date Range</label>
+		                    <div class="d-md-flex gap-2" >    
+		                        <div class="form-check">
+		                            <input class="form-check-input" type="radio" name="dateRange" id="booking">
+		                            <label class="form-check-label-sm" for="booking">
+		                                Booking
+		                            </label>
+		                        </div>
+		                        <div class="form-check">
+		                            <input class="form-check-input" type="radio" name="dateRange" id="issued">
+		                            <label class="form-check-label-sm" for="issued">
+		                                Issued
+		                            </label>
+		                        </div>
+		                        <div class="form-check">
+		                            <input class="form-check-input" type="radio" name="dateRange" id="piad">
+		                            <label class="form-check-label-sm" for="piad">
+		                                Piad
+		                            </label>
+		                        </div>
+		                    </div>
+		                    <div class="mb-4 mb-lg-0 position-relative has-date-icon-left-side">
+		                      <x-icon name="input-calender"/>
+		                      <input type="" class="form-control form-control-md js-single-date" placeholder="MM/DD/YYYY" name="selectDate" aria-label="Select Date">
+		                    </div>
+		                  </div>
+		                  <div class="mb-4 mb-lg-0">
+												<label class="form-label-sm" for="review-status-column">
+													Review Status
+												</label>
+												<select class="select2 form-select form-select-md" id="review-status-column">
+													<option>Pending</option>
+												</select>
+					                      </div>
+					                      <div class="mb-4 mb-lg-0">
+												<label class="form-label-sm" for="payment-status-column">
+													Payment Status
+												</label>
+												<select class="select2 form-select form-select-md" id="payment-status-column">
+													<option>Pending</option>
+												</select>
+		                  </div>
+		                </div>
+		              </div>
+		              <!-- END: Filters -->
+									<div class="mb-4">
+										<input class="form-check-input" type="checkbox" value="select-all-providers" id="select-all-providers">
+										<label class="form-check-label fw-semibold" for="select-all-providers">
+											Select All Providers
+										</label>
+									</div>
+						    </div>
+						  </div>
+					    <div class="row">
+					    	<div class="col-lg-6 mb-4">
+					    		<div class="dropdown">
+	                  <button class="btn btn-outline-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+	                    <svg width="23" height="26" viewBox="0 0 23 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+	                      <path d="M10 0.5V8.625C10 9.12228 10.1975 9.5992 10.5492 9.95083C10.9008 10.3025 11.3777 10.5 11.875 10.5H20V17.6963L18.3837 16.08C18.2676 15.9639 18.1298 15.8719 17.978 15.8091C17.8263 15.7463 17.6637 15.7141 17.4996 15.7141C17.3354 15.7142 17.1728 15.7466 17.0211 15.8095C16.8695 15.8723 16.7317 15.9645 16.6156 16.0806C16.4996 16.1968 16.4075 16.3346 16.3447 16.4863C16.282 16.638 16.2497 16.8006 16.2497 16.9648C16.2498 17.129 16.2822 17.2916 16.3451 17.4432C16.408 17.5949 16.5001 17.7327 16.6162 17.8488L18.0175 19.25H12.5C12.1685 19.25 11.8505 19.3817 11.6161 19.6161C11.3817 19.8505 11.25 20.1685 11.25 20.5C11.25 20.8315 11.3817 21.1495 11.6161 21.3839C11.8505 21.6183 12.1685 21.75 12.5 21.75H18.0175L16.6162 23.1513C16.3817 23.3856 16.2499 23.7036 16.2497 24.0352C16.2496 24.3668 16.3812 24.6848 16.6156 24.9194C16.85 25.1539 17.168 25.2858 17.4996 25.2859C17.8311 25.286 18.1492 25.1544 18.3837 24.92L19.9787 23.3238C19.9002 23.9256 19.6053 24.4783 19.1492 24.8787C18.6931 25.2791 18.1069 25.5 17.5 25.5H2.5C1.83696 25.5 1.20107 25.2366 0.732233 24.7678C0.263392 24.2989 0 23.663 0 23V3C0 2.33696 0.263392 1.70107 0.732233 1.23223C1.20107 0.763392 1.83696 0.5 2.5 0.5H10ZM20 17.6963L21.9187 19.6163C22.1531 19.8507 22.2847 20.1685 22.2847 20.5C22.2847 20.8315 22.1531 21.1493 21.9187 21.3838L20 23.3038V17.6963ZM12.5 0.55375C12.9736 0.654194 13.4078 0.889989 13.75 1.2325L19.2675 6.75C19.61 7.09216 19.8458 7.5264 19.9462 8H12.5V0.55375Z" fill="#023DB0"/>
+	                      </svg>
+	                  </button>
+	                  <ul class="dropdown-menu">
+	                    <li><a class="dropdown-item" href="#">Action</a></li>
+	                    <li><a class="dropdown-item" href="#">Another action</a></li>
+	                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+	                  </ul>
+	                </div>
+					    	</div>
+							  <div class="col-lg-6 text-end mb-4">
+									<a href="#" class="btn btn-primary rounded ">
+										<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="mx-2">
+											<path fill-rule="evenodd" clip-rule="evenodd" d="M10 0C4.47727 0 0 4.47727 0 10C0 15.5227 4.47727 20 10 20C15.5227 20 20 15.5227 20 10C20 4.47727 15.5227 0 10 0ZM10.9091 13.6364C10.9091 13.8775 10.8133 14.1087 10.6428 14.2792C10.4723 14.4497 10.2411 14.5455 10 14.5455C9.75889 14.5455 9.52766 14.4497 9.35718 14.2792C9.18669 14.1087 9.09091 13.8775 9.09091 13.6364V10.9091H6.36364C6.12253 10.9091 5.8913 10.8133 5.72081 10.6428C5.55032 10.4723 5.45455 10.2411 5.45455 10C5.45455 9.75889 5.55032 9.52766 5.72081 9.35718C5.8913 9.18669 6.12253 9.09091 6.36364 9.09091H9.09091V6.36364C9.09091 6.12253 9.18669 5.8913 9.35718 5.72081C9.52766 5.55032 9.75889 5.45455 10 5.45455C10.2411 5.45455 10.4723 5.55032 10.6428 5.72081C10.8133 5.8913 10.9091 6.12253 10.9091 6.36364V9.09091H13.6364C13.8775 9.09091 14.1087 9.18669 14.2792 9.35718C14.4497 9.52766 14.5455 9.75889 14.5455 10C14.5455 10.2411 14.4497 10.4723 14.2792 10.6428C14.1087 10.8133 13.8775 10.9091 13.6364 10.9091H10.9091V13.6364Z" fill="white"/>
+											</svg>
+											Add Reimbursement
+										</a>
+							  </div>
 					    </div>
-					  <div class="col-md-3 ms-auto text-end mb-4">
-						<a href="#" class="btn btn-primary rounded "><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="mx-2">
-							<path fill-rule="evenodd" clip-rule="evenodd" d="M10 0C4.47727 0 0 4.47727 0 10C0 15.5227 4.47727 20 10 20C15.5227 20 20 15.5227 20 10C20 4.47727 15.5227 0 10 0ZM10.9091 13.6364C10.9091 13.8775 10.8133 14.1087 10.6428 14.2792C10.4723 14.4497 10.2411 14.5455 10 14.5455C9.75889 14.5455 9.52766 14.4497 9.35718 14.2792C9.18669 14.1087 9.09091 13.8775 9.09091 13.6364V10.9091H6.36364C6.12253 10.9091 5.8913 10.8133 5.72081 10.6428C5.55032 10.4723 5.45455 10.2411 5.45455 10C5.45455 9.75889 5.55032 9.52766 5.72081 9.35718C5.8913 9.18669 6.12253 9.09091 6.36364 9.09091H9.09091V6.36364C9.09091 6.12253 9.18669 5.8913 9.35718 5.72081C9.52766 5.55032 9.75889 5.45455 10 5.45455C10.2411 5.45455 10.4723 5.55032 10.6428 5.72081C10.8133 5.8913 10.9091 6.12253 10.9091 6.36364V9.09091H13.6364C13.8775 9.09091 14.1087 9.18669 14.2792 9.35718C14.4497 9.52766 14.5455 9.75889 14.5455 10C14.5455 10.2411 14.4497 10.4723 14.2792 10.6428C14.1087 10.8133 13.8775 10.9091 13.6364 10.9091H10.9091V13.6364Z" fill="white"/>
-							</svg>
-							Add Reimbursement</a>
-					  </div>
-					</div>
-					</div>
+						</div>
 					</div>
 					<div class="d-flex flex-column flex-md-row justify-content-between mb-2 gap-2">
 					  <div class="d-inline-flex align-items-center gap-4">
