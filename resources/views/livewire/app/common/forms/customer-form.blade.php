cfd3dfe3a2900f0c3c5ae99ab5303df0fb348af2
Labib Muhammad Jamal <lj@ageogroup.co.uk>
2023-03-15 21:07:50 +0500

    create cstomer -> customer info tab -> iPad/Mobile screens -> updated

--- resources/views/livewire/app/common/forms/customer-form.blade.php
+++ resources/views/livewire/app/common/forms/customer-form.blade.php
@@ -1,6 +1,6 @@
 <div>
 	<div class="content-header row">
-		<div class="content-header-left col-md-9 col-12 mb-2">
+		<div class="content-header-left col-12 mb-2">
 			<div class="row breadcrumbs-top">
 				<div class="col-12">
 					<h1 class="content-header-title float-start mb-0">
