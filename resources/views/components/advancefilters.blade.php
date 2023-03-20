741d44c1525fc37c01beb8faae7782495017ff9e
Amna Bilal <amnausman@gmail.com>
2023-03-21 00:33:42 +0500

    filters component included

---
+++ resources/views/components/advancefilters.blade.php
@@ -0,0 +1,83 @@
+<div class="row">
+        <div class="col-lg-5 pe-lg-3 mb-5">
+          <label class="form-label">Filter by Accommodation</label>
+          <select data-placeholder="Select Accommodations" multiple class="form-select chosen-select" tabindex="" id="accommodation">
+            <option value=""></option>
+            <option selected>Shelby Sign Language</option>
+            <option selected>Language Translation Services</option>
+          </select>
+        </div>
+        <div class="col-lg-5 ps-lg-3 mb-5">
+          <label class="form-label">Filter by Service</label>
+          <select data-placeholder="Select Service" multiple class="form-select chosen-select" tabindex="" id="service">
+            <option value=""></option>
+            <option selected>Shelby Sign Language</option>
+            <option selected>Language Translation Services</option>
+          </select>
+        </div>
+        <div class="col-lg-2 d-flex text-nowrap align-items-center align-self-end mb-5">
+          <a class="btn btn-inactive dropdown-toggle rounded" data-bs-toggle="collapse" href="#collapseAdvanceFilter" role="button" aria-expanded="false" aria-controls="collapseAdvanceFilter">
+              <span class="">Advance Filter</span>
+          </a>
+        </div>
+      </div>
+      <div class="collapse" id="collapseAdvanceFilter">
+        <div class="col-lg-12">
+          <div class="row">
+            <div class="col-lg-5 pe-lg-3 mb-5">
+              <label class="form-label">Specialization</label>
+              <select data-placeholder="Select Specialization" multiple class="form-select chosen-select" tabindex="" id="specialization">
+                <option value=""></option>
+                <option selected>Deaf-Blind Tactile</option>
+                <option selected>Closed-Captioning</option>
+              </select>
+            </div>
+            <div class="col-lg-5 ps-lg-3 mb-5">
+              <label class="form-label">Service Type</label>
+              <select data-placeholder="Select Service Type" multiple class="form-select chosen-select" tabindex="" id="serviceType">
+                <option value=""></option>
+                <option selected>Language interpreting</option>
+                <option selected>New service capacity</option>
+              </select>
+            </div>
+            <div class="col-lg-5 pe-lg-3 mb-5">
+              <label class="form-label">Organization(s) / Departments / User(s)</label>
+              <select data-placeholder="Select Company" multiple class="form-select chosen-select" tabindex="" id="OrgDeptUser">
+                <option value=""></option>
+                <option selected>AbmaSoft</option>
+                <option selected>Dept.</option>
+                <option selected>Individual User</option>
+              </select>
+            </div>
+            <div class="col-lg-5 ps-lg-3 mb-5">
+              <label class="form-label">Provider</label>
+              <select data-placeholder="Select Provider" multiple class="form-select chosen-select" tabindex="" id="provider">
+                <option value=""></option>
+                <option selected>Chandler Leach</option>
+                <option selected>Giacomo Marsh</option>
+              </select>
+            </div>
+            <div class="col-lg-5 pe-lg-3 mb-5">
+              <label class="form-label">Tags</label>
+              <select data-placeholder="Select Tags" multiple class="form-select chosen-select" tabindex="" id="tags">
+                <option value=""></option>
+                <option selected>end_Assignment</option>
+                <option selected>end_date</option>
+              </select>
+            </div>
+            <div class="col-lg-5 ps-lg-3 mb-5">
+              <label class="form-label">Industry</label>
+              <select data-placeholder="Select Industry" multiple class="form-select chosen-select" tabindex="" id="industry">
+                <option value=""></option>
+              </select>
+            </div>
+            <div class="col-lg-5 ps-lg-3 mb-5">
+              <label class="form-label">Status</label>
+              <select data-placeholder="Select Booking Status" multiple class="form-select chosen-select" tabindex="" id="bookingStatus">
+                <option value=""></option>
+                <option selected>Booking Status</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
