7ada4218318929a9bf8073694d17d1e292577373
shanila <shanilla@abmasoft.com>
2023-03-21 05:40:51 +0000

    added models in remittance accept and objection model

---
+++ resources/views/modals/accept-remittance.blade.php
@@ -0,0 +1,21 @@
+<div class="modal fade" id="acceptModal" tabindex="-1" aria-labelledby="acceptModal" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="objectionModal">Accept Invoice</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
+                    onclick="closeModal()"></button>
+            </div>
+            <div class="modal-body">
+                <div><label class="form-label" for="address-title">Notes</label></div>
+
+                <textarea placeholder="Enter Text Here" name="" id="" cols="50" rows="5"></textarea>
+            </div>
+            <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary">Cancel</button>
+                    <button type="button" class="btn btn-primary">Accept</button>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal-backdrop fade show" id="backdrop" style="display: none;"></div>
