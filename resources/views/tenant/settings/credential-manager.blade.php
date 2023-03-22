1a195e561a552b4cb113f992317e2a255770f40c
Safia Liquat <assistabmasoft@gmail.com>
2023-03-22 06:48:34 +0000

    Created credential Manager section

--- resources/views/tenant/settings/credential-manager.blade.php
+++ resources/views/tenant/settings/credential-manager.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.tenant', ['title' => 'Settings'])
 
 @section('content')
-	<x-coming-soon moduleName="Credential Manager"></x-coming-soon>
+   @livewire('app.common.forms.credential-manager')
 @endsection
