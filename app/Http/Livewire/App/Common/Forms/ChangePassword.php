a089bb7c7e119bac618567bfc07706e598d82ef1
Sohail Asghar <sohail@abmasoft.com>
2023-03-23 10:26:10 +0000

    Activated change password form for admin

--- app/Http/Livewire/App/Common/Forms/ChangePassword.php
+++ app/Http/Livewire/App/Common/Forms/ChangePassword.php
@@ -2,26 +2,63 @@
 
 namespace App\Http\Livewire\App\Common\Forms;
 
+use App\Models\Tenant\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
 class ChangePassword extends Component
 {
-    public $showForm;
-    protected $listeners = ['showList'=>'resetForm'];
+	public $current_password;
+	public $password;
+	public $password_confirmation;
 
-    public function mount() {}
+	protected $messages = [
+		'password.regex' => 'Password must have at least one uppercase letter, one number and one special character ',
+	];
 
-    function showForm()
-    {
-       $this->showForm=true;
-    }
-    public function resetForm()
-    {
-        $this->showForm=false;
-    }
+	public function changePassword()
+	{
+		$user = User::findOrFail(Auth::user()->id);
+		$this->validate([
+			'current_password' => ['required', function ($attribute, $value, $fail) use ($user) {
+				if (!Hash::check($value, $user->password)) {
+					return $fail(__('The current password is incorrect.'));
+				}
+			}],
+			'password' => [
+				'required',
+				'string',
+				'min:8',
+				'regex:/[A-Z]/', // Must have at least one uppercase letter
+				'regex:/[0-9]/', // Must have at least one number
+				'regex:/[!@#$%^&*(),.?":{}|<>]/', // Must have at least one special character
+				'confirmed',
+			],
+		]);
+		
+		if(Hash::check($this->current_password, $user->password))
+		{
+			$user->password = Hash::make($this->password);
+			$user->save();
+			$this->dispatchBrowserEvent('swal:modal', [
+				'type' => 'success',
+				'title' => 'Success',
+				'text' => 'Password updated successfully!',
+			]);
+			$this->resetFields();
+		}
+	}
 
-    public function render()
-    {
-        return view('livewire.app.common.forms.change-password');
-    }
+	private function resetFields()
+	{
+		$this->current_password = '';
+		$this->password = '';
+		$this->password_confirmation = '';
+	}
+
+	public function render()
+	{
+		return view('livewire.app.common.forms.change-password');
+	}
 }
