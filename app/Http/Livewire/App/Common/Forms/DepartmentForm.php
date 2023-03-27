03a585d0eeaa8705f8e792e58004042c5339eb76
Safia Liquat <assistabmasoft@gmail.com>
2023-03-27 06:27:10 +0000

    Fixed department form and added missing panels and models

--- app/Http/Livewire/App/Common/Forms/DepartmentForm.php
+++ app/Http/Livewire/App/Common/Forms/DepartmentForm.php
@@ -6,6 +6,7 @@ use Livewire\Component;
 
 class DepartmentForm extends Component
 {
+	public $component = 'department-info';
 	public function showList()
 	{
 		$this->emit('showList');
@@ -13,6 +14,11 @@ class DepartmentForm extends Component
 
 	public function mount()
 	{}
+
+	public function switch($component)
+	{
+		$this->component = $component;
+	}
 
 	public function render()
 	{
