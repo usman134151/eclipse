88d1098de7dde46f23e25469b484cdaba9fe90ac
unknown <amnausman@gmail.com>
2023-03-29 10:32:39 +0500

    Provider seeder

--- database/seeders/TenantDatabaseSeeder.php
+++ database/seeders/TenantDatabaseSeeder.php
@@ -32,6 +32,7 @@ class TenantDatabaseSeeder extends Seeder
       $this->call(BookingsSeeder::class); //added records in booking table by Sohail
       $this->call(SetupSeeder::class); //added records in setup table by Amna Bilal
       $this->call(SetupValuesSeeder::class); //added records in setup values table by Amna Bilal
+      $this->call(ProviderSeeder::class); //added records for providers table by Amna Bilal
       
     }
 
