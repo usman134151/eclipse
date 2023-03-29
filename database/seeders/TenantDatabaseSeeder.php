043e8b889b5010b7544d4a43289aa3f4958012bd
shanila <shanilla@abmasoft.com>
2023-03-29 10:44:07 +0000

    created seeder for customer

--- database/seeders/TenantDatabaseSeeder.php
+++ database/seeders/TenantDatabaseSeeder.php
@@ -33,7 +33,8 @@ class TenantDatabaseSeeder extends Seeder
       $this->call(SetupSeeder::class); //added records in setup table by Amna Bilal
       $this->call(SetupValuesSeeder::class); //added records in setup values table by Amna Bilal
       $this->call(ProviderSeeder::class); //added records for providers table by Amna Bilal
-      
+      $this->call(CustomerSeeder::class); //added records for customer table by Shanilla Wali
+
     }
 
 }
