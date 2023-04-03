0aa92c1cf3577fb9f00b7b000ec9f0675a74f1d2
unknown <amnausman@gmail.com>
2023-04-04 01:30:10 +0500

    Navigator

---
+++ database/migrations/tenant/2023_04_03_193237_create_navigator_user_navigator_tables.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNavigatorUserNavigatorTables extends Migration
+{
+    public function up()
+    {
+        Schema::create('navigators', function (Blueprint $table) {
+            $table->id();
+            $table->string('navigator_label');
+            $table->string('navigator_icon');
+            $table->string('navigator_link');
+            $table->timestamps();
+        });
+
+        Schema::create('user_navigator', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('navigator_id')->constrained()->onDelete('cascade');
+            $table->integer('position');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('user_navigator');
+        Schema::dropIfExists('navigators');
+    }
+}
