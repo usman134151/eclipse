00e9d08d05930ea4302e9db792cd7cfe3ceb68f1
unknown <amnausman@gmail.com>
2023-04-04 02:24:59 +0500

    Navigator seeder

---
+++ app/Models/Tenant/Navigator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Tenant;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Navigator extends Model
+{
+    use HasFactory;
+}
