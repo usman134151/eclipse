eaedbe282bbc7bb3d1944215a2096ef3c4e0eae1
Labib Muhammad Jamal <lj@ageogroup.co.uk>
2023-04-04 23:44:01 +0500

    flatpickr & bootstrap-select.min cdn css/js files downloaded -> placed within /public/js & /public/css folder respectively

--- config/livewire-powergrid.php
+++ config/livewire-powergrid.php
@@ -28,15 +28,15 @@ return [
          * https://github.com/snapappointments/bootstrap-select
          */
         'bootstrap-select' => [
-            'js'  => 'https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta2/dist/js/bootstrap-select.min.js',
-            'css' => 'https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta2/dist/css/bootstrap-select.min.css',
+            'js'  => '/public/js/bootstrap-select.min.js',
+            'css' => '/public/css/bootstrap-select.min.css',
         ],
         /*
          * https://flatpickr.js.org
          */
         'flatpickr'        => [
-            'js'      => 'https://cdn.jsdelivr.net/npm/flatpickr',
-            'css'     => 'https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css',
+            'js'      => '',
+            'css'     => '/public/css/flatpickr.min.css',
             'locales' => [
                 'pt_BR' => [
                     'locale'     => 'pt',
