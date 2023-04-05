021ca4f492df31a0997d642ab68ee4d2c84a52b0
Labib Muhammad Jamal <lj@ageogroup.co.uk>
2023-04-05 23:11:06 +0500

    header -> admin link moved/integrated

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -62,17 +62,6 @@
         </div>
         <!-- END: search-by-keyword - search-by-no -->
         <ul class="nav navbar-nav align-items-center ms-auto">
-        <li class="nav-item dropdown dropdown-user">
-              <div class="user-nav d-sm-flex d-none">
-                <span class="user-name fw-medium m-1">
-                  {{-- Interpreter Admin --}}
-                  {{ Auth::user()->first_name}} {{ Auth::user()->last_name }}
-                </span>
-              </div>
-            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                @csrf
-              </form>
-          </li>
           <li class="nav-item dropdown dropdown-notification me-25">
                         <a href="#" aria-label="Eclipse Scheduling Notification" id="dropdownNotification" class="nav-link" data-bs-toggle="dropdown"><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" clip-rule="evenodd" d="M19.1667 14.1668C19.1667 14.6668 18.8334 15.0002 18.3334 15.0002H1.66671C1.16671 15.0002 0.833374 14.6668 0.833374 14.1668C0.833374 13.6668 1.16671 13.3335 1.66671 13.3335C2.58337 13.3335 3.33337 12.5835 3.33337 11.6668V7.50016C3.33337 3.8335 6.33337 0.833496 10 0.833496C13.6667 0.833496 16.6667 3.8335 16.6667 7.50016V11.6668C16.6667 12.5835 17.4167 13.3335 18.3334 13.3335C18.8334 13.3335 19.1667 13.6668 19.1667 14.1668ZM12.1667 17.9168C11.6667 18.7502 10.8334 19.1668 10 19.1668C9.58337 19.1668 9.16671 19.0835 8.75004 18.8335C8.33337 18.5835 8.08337 18.3335 7.83337 17.9168C7.58337 17.5002 7.75004 17.0002 8.16671 16.7502C8.58337 16.5002 9.08337 16.6668 9.33337 17.0835C9.3586 17.1087 9.38383 17.1416 9.41137 17.1775L9.41138 17.1775C9.47481 17.2601 9.5505 17.3587 9.66671 17.4168C10.0834 17.6668 10.5834 17.5002 10.8334 17.0835C11.0834 16.6668 11.5834 16.5835 12 16.7502C12.4167 16.9168 12.4167 17.5002 12.1667 17.9168ZM15 11.6668C15 12.2502 15.1667 12.8335 15.4167 13.3335H4.58337C4.83337 12.8335 5.00004 12.2502 5.00004 11.6668V7.50016C5.00004 4.75016 7.25004 2.50016 10 2.50016C12.75 2.50016 15 4.75016 15 7.50016V11.6668Z" fill="black"/>
@@ -112,15 +101,26 @@
               onclick="event.preventDefault();
               document.getElementById('logout-form').submit();">
               <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M3.8385 2.1263L4.8708 3.6014C3.61636 4.47959 2.67458 5.73485 2.18227 7.18484C1.68995 8.63482 1.67274 10.204 2.13314 11.6644C2.59354 13.1249 3.50756 14.4005 4.74244 15.306C5.97731 16.2115 7.46872 16.6997 9 16.6997C10.5313 16.6997 12.0227 16.2115 13.2576 15.306C14.4924 14.4005 15.4065 13.1249 15.8669 11.6644C16.3273 10.204 16.31 8.63482 15.8177 7.18484C15.3254 5.73485 14.3836 4.47959 13.1292 3.6014L14.1615 2.1263C15.3477 2.95558 16.316 4.05901 16.9843 5.34278C17.6526 6.62655 18.001 8.0527 18 9.5C18 14.4707 13.9707 18.5 9 18.5C4.0293 18.5 2.33121e-06 14.4707 2.33121e-06 9.5C-0.00103989 8.0527 0.347392 6.62655 1.01568 5.34278C1.68397 4.05901 2.65235 2.95558 3.8385 2.1263ZM8.1 9.5V0.5H9.9V9.5H8.1Z" fill="#6E6B7B"/>
-          </svg></a></li>
-          <li class="nav-item dropdown dropdown-user"><a class="nav-link dropdown-toggle dropdown-user-link" id="dropdown-user" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <div class="user-nav d-sm-flex d-none"><span class="user-name fw-bolder">John Doe</span><span class="user-status">Admin</span></div><span class="avatar"><img class="round" src="/html-prototype/images/portrait/small/avatar-s-11.jpg" alt="avatar" height="40" width="40"><span class="avatar-status-online"></span></span>
-              </a>
-              
+                <path d="M3.8385 2.1263L4.8708 3.6014C3.61636 4.47959 2.67458 5.73485 2.18227 7.18484C1.68995 8.63482 1.67274 10.204 2.13314 11.6644C2.59354 13.1249 3.50756 14.4005 4.74244 15.306C5.97731 16.2115 7.46872 16.6997 9 16.6997C10.5313 16.6997 12.0227 16.2115 13.2576 15.306C14.4924 14.4005 15.4065 13.1249 15.8669 11.6644C16.3273 10.204 16.31 8.63482 15.8177 7.18484C15.3254 5.73485 14.3836 4.47959 13.1292 3.6014L14.1615 2.1263C15.3477 2.95558 16.316 4.05901 16.9843 5.34278C17.6526 6.62655 18.001 8.0527 18 9.5C18 14.4707 13.9707 18.5 9 18.5C4.0293 18.5 2.33121e-06 14.4707 2.33121e-06 9.5C-0.00103989 8.0527 0.347392 6.62655 1.01568 5.34278C1.68397 4.05901 2.65235 2.95558 3.8385 2.1263ZM8.1 9.5V0.5H9.9V9.5H8.1Z" fill="#6E6B7B"/>
+              </svg>
+            </a>
           </li>
-
-
+          <li class="nav-item dropdown dropdown-user">
+            <a class="nav-link dropdown-toggle dropdown-user-link" id="dropdown-user" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <div class="user-nav d-sm-flex d-none">
+                {{-- Interpreter Admin --}}
+                <span class="user-name fw-bolder">{{ Auth::user()->first_name}}</span>
+                <span class="user-status">{{ Auth::user()->last_name }}</span>
+              </div>
+              <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                @csrf
+              </form>
+              <span class="avatar">
+                <img class="round" src="/html-prototype/images/portrait/small/avatar-s-11.jpg" alt="avatar" height="40" width="40">
+                <span class="avatar-status-online"></span>
+              </span>
+            </a>
+          </li>
         </ul>
       </div>
     </nav>
