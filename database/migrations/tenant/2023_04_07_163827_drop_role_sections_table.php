d153bd4aebe4d3ebeb175a3370f4d160e3a74fbc
Sohail Asghar <sohail@abmasoft.com>
2023-04-07 17:06:23 +0000

    Added migration to drop role_sections table

---
+++ database/migrations/tenant/2023_04_07_163827_drop_role_sections_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('role_sections', function (Blueprint $table) {
+			$table->dropForeign(['system_role_id']);
+			$table->dropForeign(['section_right_id']);
+		});
+		Schema::dropIfExists('role_sections');
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::create('role_sections', function (Blueprint $table) {
+			$table->id();
+			$table->biginteger('system_role_id')->unsigned();
+			$table->biginteger('section_right_id')->unsigned();
+			$table->timestamps();
+		});
+		Schema::table('role_sections', function($table) {
+			$table->foreign('system_role_id')->references('system_role_id')->on('system_roles')->onDelete('cascade');
+			$table->foreign('section_right_id')->references('id')->on('section_rights')->onDelete('cascade');
+		});
+	}
+};
