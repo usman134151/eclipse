fad25e10edc91ad2e60e1fe76c014df4d7820c8b
Labib Muhammad Jamal <lj@ageogroup.co.uk>
2023-04-11 02:32:58 +0500

    add company -> mobile responsiveness ->  form action buttons & Drive documents -> updated

--- resources/views/livewire/app/admin/forms/add-company.blade.php
+++ resources/views/livewire/app/admin/forms/add-company.blade.php
@@ -1050,7 +1050,7 @@
                                                 </div>
                                             </div>
 
-                                            <div class="col-12 justify-content-center form-actions d-flex gap-2">
+                                            <div class="col-12 justify-content-center form-actions d-flex flex-column flex-md-row gap-2">
                                                 <button type="button" class="btn btn-outline-dark rounded px-4 py-2"
                                                     x-on:click="$wire.switch('company-info')">
                                                     Back
@@ -1088,9 +1088,9 @@
                                             <div class="col-md-12 mb-md-2">
                                                 <div class="row">
                                                     <div class="col-md-12 mb-md-2">
-                                                        <div class="d-flex justify-content-center">
+                                                        <div class="d-flex justify-content-md-center">
                                                             <div>
-                                                                <h3 class="text-center">
+                                                                <h3 class="text-md-center">
                                                                     Upload Document
                                                                 </h3>
                                                                 <input class="form-control form-control-sm" type="file"
@@ -1099,7 +1099,7 @@
                                                         </div>
                                                     </div>
                                                 </div>
-                                                <div class="d-flex justify-content-center gap-3 mt-5">
+                                                <div class="d-md-flex justify-content-center gap-3 mt-5">
                                                     <div>
                                                         <img src="/tenant/images/img-placeholder-document.jpg"
                                                             alt="Preview File" />
@@ -1117,7 +1117,7 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div class="col-12 justify-content-center form-actions d-flex">
+                                            <div class="col-12 justify-content-center form-actions d-flex flex-column flex-md-row gap-2">
                                                 <button type="button"
                                                     class="btn btn-outline-dark rounded px-4 py-2 mx-2"
                                                     x-on:click="$wire.switch('service-catalog')">
