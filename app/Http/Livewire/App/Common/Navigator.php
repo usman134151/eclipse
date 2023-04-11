d48f2567296fe51e8bd4259233f93432819b7b9f
unknown <amnausman@gmail.com>
2023-04-11 11:03:39 +0500

    Navigator count - temp

--- app/Http/Livewire/App/Common/Navigator.php
+++ app/Http/Livewire/App/Common/Navigator.php
@@ -16,7 +16,40 @@ class Navigator extends Component
 
     public function render()
     {   
-        $this->userNavigators = $this->getNavigators();     
+        $this->userNavigators = $this->getNavigators();   
+          
+        $navigators = [];
+
+        foreach ($this->userNavigators as $userNavigator) {
+            $navigators[] = [
+                'navigator_id' => $userNavigator->navigator_id ?? $userNavigator->id,
+                'navigator_label' => $userNavigator->Navigator->navigator_label ?? $userNavigator->navigator_label,
+                'navigator_icon' => $userNavigator->Navigator->navigator_icon ?? $userNavigator->navigator_icon,
+                'navigator_link' => $userNavigator->Navigator->navigator_link ?? $userNavigator->navigator_link,
+            ];
+        }
+    
+        $counter = [
+            ['navigator_id'=>1,'count'=>47],
+            ['navigator_id'=>2,'count'=>29],
+            ['navigator_id'=>3,'count'=>42],
+            ['navigator_id'=>4,'count'=>66],
+            ['navigator_id'=>5,'count'=>18],
+            ['navigator_id'=>6,'count'=>11],
+            ['navigator_id'=>7,'count'=>25],
+            ['navigator_id'=>8,'count'=>15],
+            ['navigator_id'=>9,'count'=>9],
+            ['navigator_id'=>10,'count'=>15]
+        ];
+    
+        $this->userNavigators = array_map(function ($navigator) use ($counter) {
+            $key = array_search($navigator['navigator_id'], array_column($counter, 'navigator_id'));
+            if ($key !== false) {
+                $navigator['count'] = $counter[$key]['count'];
+            }
+            return $navigator;
+        }, $navigators);
+      
         return view('livewire.app.common.navigator');
     }
 
--- resources/views/livewire/app/common/navigator.blade.php
+++ resources/views/livewire/app/common/navigator.blade.php
@@ -5,42 +5,25 @@
 
 	<ul wire:sortable="updateNavigateOrder" class="d-grid 2xl:grid-cols-6 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 list-unstyled mb-5" >	
 
- 		@isset($userNavigators[0]->navigator_id)
+ 		
 				@foreach($userNavigators as $userNavigator)   
-					  <li role="presentation" wire:sortable.item="{{ $userNavigator->navigator_id }}" wire:key="navigation-{{ $userNavigator->navigator_id }}" >
-						<a class="dashborad-navigator-block" href="{{ $userNavigator->Navigator->navigator_link}}" type="button">
-						  <div class="text-center block-text">{{ $userNavigator->Navigator->navigator_label }}</div>
-						  <div class="text-center block-icon">
-							<svg class="fill" width="72" height="64" viewBox="0 0 72 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<use xlink:href="{{ $userNavigator->Navigator->navigator_icon }}"></use>
-							</svg>
-						  </div>
-						  <div>
-							<div class="text-center block-number">50</div>
-						  </div>
-						</a>
-					  </li>
+				<li role="presentation" wire:sortable.item="{{ $userNavigator['navigator_id'] }}" wire:key="navigation-{{ $userNavigator['navigator_id'] }}">
+                <a class="dashborad-navigator-block" href="{{ $userNavigator['navigator_link'] }}" type="button">
+                    <div class="text-center block-text">{{ $userNavigator['navigator_label'] }}</div>
+                    <div class="text-center block-icon">
+                        <svg class="fill" width="72" height="64" viewBox="0 0 72 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <use xlink:href="{{ $userNavigator['navigator_icon'] }}"></use>
+                        </svg>
+                    </div>
+                    <div>
+                        <div class="text-center block-number">{{ $userNavigator['count'] }}</div>
+                    </div>
+                </a>
+            </li>
 				@endforeach
- 		@endisset
+ 		
 
- 		@isset($userNavigators[0]->navigator_label)
-				@foreach($userNavigators as $userNavigator)   
-					  <li role="presentation" wire:sortable.item="{{ $userNavigator->id }}" wire:key="navigation-{{ $userNavigator->id }}" >
-						<a class="dashborad-navigator-block" href="{{ $userNavigator->navigator_link}}" type="button">
-						  <div class="text-center block-text">{{ $userNavigator->navigator_label }}</div>
-						  <div class="text-center block-icon">
-							<svg class="fill" width="72" height="64" viewBox="0 0 72 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<use xlink:href="{{ $userNavigator->navigator_icon }}"></use>
-							</svg>
-						  </div>
-						  <div>
-							<div class="text-center block-number">50</div>
-						  </div>
-						</a>
-					  </li>
-				@endforeach
- 		@endisset
-
+ 		
  	{{-- End of update by waqar mughal --}}
 
 	</ul>
