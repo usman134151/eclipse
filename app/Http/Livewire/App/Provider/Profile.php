3939090fc387338f7f977d29d38f72d614e9d3e9
shanila <shanilla@abmasoft.com>
2023-04-19 17:55:48 +0000

    added dropdown for languages,timezone,country,gender and ethencity in profile in provider account

--- app/Http/Livewire/App/Provider/Profile.php
+++ app/Http/Livewire/App/Provider/Profile.php
@@ -3,12 +3,20 @@
 namespace App\Http\Livewire\App\Provider;
 
 use Livewire\Component;
+use App\Helpers\SetupHelper;
 
 class Profile extends Component
 {
     public $showForm;
     protected $listeners = ['showList' => 'resetForm'];
     public $component = 'profile';
+    public $setupValues = [
+        'languages' => ['parameters' => ['SetupValue', 'id','setup_value_label','setup_id',1,'setup_value_label',false,'languages', '','languages']],
+        'ethnicities' => ['parameters' => ['SetupValue', 'id','setup_value_label', 'setup_id', 3, 'setup_value_label', false,'ethnicity','','ethnicity']],
+        'gender' => ['parameters' => ['SetupValue', 'id','setup_value_label', 'setup_id', 2, 'setup_value_label', false,'gender','','gender']],
+        'timezones' => ['parameters' => ['SetupValue', 'id','setup_value_label', 'setup_id', 4, 'setup_value_label', false,'timezone','','timezone']],
+        'countries' => ['parameters' => ['Country', 'id', 'name', '', '', 'name', false, 'country','','country']]
+	];
     public function render()
     {
         return view('livewire.app.provider.profile');
@@ -16,7 +24,7 @@ class Profile extends Component
 
     public function mount()
     {
-
+        $this->loadSetupValues();
 
     }
 
@@ -31,6 +39,14 @@ class Profile extends Component
     public function switch($component)
 	{
 		$this->component = $component;
+	}
+    public function loadSetupValues(){ //added by shanila to function to get all setup values rendered on mount
+		foreach($this->setupValues as $key=>$setupValue){
+
+			$this->setupValues[$key]['rendered'] = SetupHelper::createDropDown($setupValue['parameters'][0], $setupValue['parameters'][1],$setupValue['parameters'][2], $setupValue['parameters'][3], $setupValue['parameters'][4], $setupValue['parameters'][5], $setupValue['parameters'][6],$setupValue['parameters'][7],$setupValue['parameters'][8],$setupValue['parameters'][9]);
+		}
+
+
 	}
 
 }
