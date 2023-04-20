271ccf414d38db599d75dc4caf6530f474968a0e
shanila <shanilla@abmasoft.com>
2023-04-20 06:13:26 +0000

    added dropdown for accomodation  in add coupon form

--- app/Http/Livewire/App/Common/Forms/CouponForm.php
+++ app/Http/Livewire/App/Common/Forms/CouponForm.php
@@ -1,19 +1,35 @@
 <?php
 
 namespace App\Http\Livewire\App\Common\Forms;
+use App\Helpers\SetupHelper;
 
 use Livewire\Component;
 
 class CouponForm extends Component
 {
+    public $setupValues = [
+        'accomodations' => ['parameters' => ['Accommodation', 'id', 'name', '', '', 'name', false, 'accommodation','','accommodation']]
+	];
 	public function showList()
 	{
 		$this->emit('showList');
 	}
+    public function mount()
+    {
+        $this->loadSetupValues();
 
+    }
 	public function render()
 	{
 		return view('livewire.app.common.forms.coupon-form');
+	}
+    public function loadSetupValues(){ //added by shanila to add function to get all setup values rendered on mount
+		foreach($this->setupValues as $key=>$setupValue){
+
+			$this->setupValues[$key]['rendered'] = SetupHelper::createDropDown($setupValue['parameters'][0], $setupValue['parameters'][1],$setupValue['parameters'][2], $setupValue['parameters'][3], $setupValue['parameters'][4], $setupValue['parameters'][5], $setupValue['parameters'][6],$setupValue['parameters'][7],$setupValue['parameters'][8],$setupValue['parameters'][9]);
+		}
+
+
 	}
 
 }
