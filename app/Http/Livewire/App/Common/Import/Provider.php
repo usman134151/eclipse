25f484d3f33701fc1a1199b232cd56ec9c658682
shanila <shanilla@abmasoft.com>
2023-05-15 08:43:53 +0000

    mapping feilds of provider

--- app/Http/Livewire/App/Common/Import/Provider.php
+++ app/Http/Livewire/App/Common/Import/Provider.php
@@ -29,8 +29,25 @@ class Provider extends Component
         foreach ($rows as $row) {
             if($i>0){
                 $user = [];
-            
-
+                $user['first_name'] = $row[0];
+                $user['last_name'] = $row[1];
+                $user['title'] = $row[2];
+                $user['dob'] = $row[3];
+                $user['email'] = $row[4];
+                $user['userDetails']['language_id']=$row[5];
+                $user['userDetails']['gender_id']=$row[6];
+                $user['userDetails']['ethnicity_id']=$row[7];
+                $user['userDetails']['user_id']=$row[8];
+                $user['userDetails']['phone']=$row[9];
+                $user['userDetails']['country']=$row[10];
+                $user['userDetails']['state']=$row[11];
+                $user['userDetails']['city']=$row[12];
+                $user['userDetails']['zip']=$row[13];
+                $user['userDetails']['address_line1']=$row[14];
+                $user['userDetails']['address_line2']=$row[15];
+                $user['userDetails']['education']=$row[16];
+                $user['providerDetails']['experience']=$row[17];
+                $user['userDetails']['note']=$row[18];
                 dd($user);
                 $this->users[] = $user;
 
@@ -38,13 +55,13 @@ class Provider extends Component
             $i++;
 
         }
-       
+
     }
 
     public function save()
     {
         //dd($this->users);
-        
+
 //now this is normal array again so have to reconvert it to user model object and call user services function
         foreach($this->users as $userData){
             $user=new User();
