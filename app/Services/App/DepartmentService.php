3eab33f83305a8f5af5d147b979e57c0d30acd68
shanila <shanilla@abmasoft.com>
2023-05-16 11:43:33 +0000

    activated phones in department form

--- app/Services/App/DepartmentService.php
+++ app/Services/App/DepartmentService.php
@@ -1,11 +1,15 @@
 <?php
 namespace app\Services\App;
 use App\Models\Department;
-
+use App\Models\Tenant\Phone;
 class DepartmentService{
 
-    public function createDeparment($department){
+    public function createDeparment($department,$phones){
         $department->save();
+        foreach ($phones as $phoneData) {
+            $phone = new Phone($phoneData);
+            $department->phones()->save($phone);
+        }
         return $department;
     }
 }
