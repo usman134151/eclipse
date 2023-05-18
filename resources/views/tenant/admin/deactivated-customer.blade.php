3a950aa599ec98231917b05d313ac97908ef1737
Amna Bilal <amnausman@gmail.com>
2023-05-18 20:46:58 +0500

    customer list fix

--- resources/views/tenant/admin/deactivated-customer.blade.php
+++ resources/views/tenant/admin/deactivated-customer.blade.php
@@ -3,6 +3,6 @@
 @section('content')
 
 <!-- BEGIN: Content-->
-  @livewire('app.admin.deactivated-customer',[$status=>0])
+  @livewire('app.admin.deactivated-customer',["status"=>0])
  <!-- End: Content-->
 @endsection
