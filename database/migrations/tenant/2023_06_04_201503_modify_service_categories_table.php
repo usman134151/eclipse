c71d11584367dd9ed4e4eb54622388451d579686
Amna Bilal <amnausman@gmail.com>
2023-06-05 01:15:57 +0500

    Services step 1 - migration default

---
+++ database/migrations/tenant/2023_06_04_201503_modify_service_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('service_categories', function (Blueprint $table) {
+            $table->boolean('disable_for_companies')->default(0)->change();
+            $table->unsignedInteger('minimum_assistance_min')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('service_categories', function (Blueprint $table) {
+            $table->boolean('disable_for_companies')->change();
+            $table->unsignedInteger('minimum_assistance_min')->change();
+        });
+    }
+};
