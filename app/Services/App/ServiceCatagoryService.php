72cd2b60fe10ee16842b8ca500a1ac1888a86087
Amna Bilal <amnausman@gmail.com>
2023-06-12 18:43:13 +0500

    null check fix

--- app/Services/App/ServiceCatagoryService.php
+++ app/Services/App/ServiceCatagoryService.php
@@ -15,25 +15,27 @@ class ServiceCatagoryService{
             foreach ($specializationRecords as $record) {
             
              $specialization_id = $record['specialization_id'];
-     
+             if($specialization_id>0) {
              // Define the attributes to update or create
              $attributes = [
-                 'specialization_price' => json_encode([$record['specialization_price']]),
-                 'specialization_price_v' => json_encode([$record['specialization_price_v']]),
-                 'specialization_price_p' => json_encode([$record['specialization_price_p']]),
-                 'specialization_price_t' => json_encode([$record['specialization_price_t']]),
-                 'specialization_id' => $record['specialization_id'],
-                 'service_id'=>$service->id,
-                 'added_by'=>Auth::id()
-             ];
-           
-             // Find the ServiceSpecialization record based on specialization_id and service_id
-             // If it exists, update it with the attributes; otherwise, create a new record
-             ServiceSpecialization::insert(
-                 
-                 $attributes
-             );
-         }
+                'specialization_price' => json_encode([$record['specialization_price']]),
+                'specialization_price_v' => json_encode([$record['specialization_price_v']]),
+                'specialization_price_p' => json_encode([$record['specialization_price_p']]),
+                'specialization_price_t' => json_encode([$record['specialization_price_t']]),
+                'specialization_id' => $record['specialization_id'],
+                'service_id'=>$service->id,
+                'added_by'=>Auth::id()
+            ];
+          
+            // Find the ServiceSpecialization record based on specialization_id and service_id
+            // If it exists, update it with the attributes; otherwise, create a new record
+            ServiceSpecialization::insert(
+                
+                $attributes
+            );
+        }
+             }  
+
         }
 
         return $service;
