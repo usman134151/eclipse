3898e5a32483c7cf6f656e17a2e77e5644461f27
unknown <amnausman@gmail.com>
2023-06-20 16:40:37 +0500

    bussiness setup

---
+++ app/Services/App/ScheduleService.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\Services\App;
+
+
+use App\Models\Tenant\Schedule;
+use App\Models\Tenant\ScheduleTimeSlot;
+use App\Models\Tenant\ScheduleHoliday;
+class ScheduleService{
+
+    public function storeSchedule($schedule,$recordId,$recordType){
+       
+    }
+
+
+}
