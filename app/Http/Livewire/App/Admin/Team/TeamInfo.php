dad7d26f1b371c01dea16449306384698cf76c14
Amna Bilal <amnausman@gmail.com>
2023-06-26 03:34:52 +0500

    minor fixes

--- app/Http/Livewire/App/Admin/Team/TeamInfo.php
+++ app/Http/Livewire/App/Admin/Team/TeamInfo.php
@@ -151,6 +151,6 @@ class TeamInfo extends Component
 
 	public function previewStorageDisk()
     {
-        return '/tenant'.tenant('id').'/app/livewire-tmp/'; // Specify the name of the storage disk you want to use
+        return public_path('/tenant'.tenant('id').'/app/livewire-tmp/'); // Specify the name of the storage disk you want to use
     }
 }
