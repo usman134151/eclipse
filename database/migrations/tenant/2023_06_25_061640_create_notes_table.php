57fa5506b4040fc3e69f5be54fe3e91252dae3fc
Maarooshaa Asim <maarooshaa@gmail.com>
2023-06-25 12:58:13 +0500

    notes module completed and attached for companies

---
+++ database/migrations/tenant/2023_06_25_061640_create_notes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('record_id')->nullable();
+            $table->integer('record_type')->nullable();
+            $table->text('notes_text')->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+};
