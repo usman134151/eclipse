a6815a571a3bf05904614eef3a945e5ed7d3f2cd
Maarooshaa Asim <maarooshaa@gmail.com>
2023-06-27 11:33:16 +0500

    provider form fix

--- resources/views/livewire/app/common/forms/provider-form.blade.php
+++ resources/views/livewire/app/common/forms/provider-form.blade.php
@@ -95,7 +95,7 @@
                                                     @enderror
                                                 </div>
                                         </div>
-                                   
+                                    </div>
                                 </div>
                                 <div class="row">
                                     <div class="col-lg-12 mb-4">
