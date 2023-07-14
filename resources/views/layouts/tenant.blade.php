71c189bf2777ae4d8ef999f0fcbe1194ceda268b
Maarooshaa Asim <maarooshaa@gmail.com>
2023-07-14 16:49:54 +0500

    Activation for autofill for form-address-fields

--- resources/views/layouts/tenant.blade.php
+++ resources/views/layouts/tenant.blade.php
@@ -113,12 +113,18 @@
       var billingPlace;
 
       var billingAutocomplete = new google.maps.places.Autocomplete(document.getElementById('billing_address'));
+      var formAddressAutocomplete = new google.maps.places.Autocomplete(document.getElementById('billing_address_form'));
 
 
       google.maps.event.addListener(billingAutocomplete, 'place_changed', function() {
         billingPlace = billingAutocomplete.getPlace();
         fillBillingAddressFields();
       });
+
+		google.maps.event.addListener(formAddressAutocomplete, 'place_changed', function() {
+				billingPlace = formAddressAutocomplete.getPlace();
+				fillFormAddressFields();
+			});
 
 		function updateAddress(attrName,val){
 			Livewire.emit('updateAddressValues', attrName, val);
@@ -132,6 +138,16 @@
 		updateAddress("state",  getAddressComponent(billingPlace, 'administrative_area_level_1') || '');
 		updateAddress("country",  getAddressComponent(billingPlace, 'country') || '');
 		updateAddress("zip",  getAddressComponent(billingPlace, 'postal_code') || '');
+	  }
+	
+	  function fillFormAddressFields() {
+		//emit changes to livewire address component
+
+		updateVal("address_line1", (billingPlace.name || ''));
+		updateVal("city",  getAddressComponent(billingPlace, 'locality') || '');
+		updateVal("state",  getAddressComponent(billingPlace, 'administrative_area_level_1') || '');
+		updateVal("country_id",  getAddressComponent(billingPlace, 'country') || '');
+		updateVal("zip",  getAddressComponent(billingPlace, 'postal_code') || '');
 	  }
 
       function getAddressComponent(place, component) {
