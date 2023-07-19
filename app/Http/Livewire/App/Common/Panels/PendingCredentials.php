4e2f90471a2736e095c28d014ebc51aca172b218
Maarooshaa Asim <maarooshaa@gmail.com>
2023-07-19 18:52:48 +0500

    document display in panel changes

--- app/Http/Livewire/App/Common/Panels/PendingCredentials.php
+++ app/Http/Livewire/App/Common/Panels/PendingCredentials.php
@@ -3,11 +3,12 @@
 namespace App\Http\Livewire\App\Common\Panels;
 
 use App\Models\Tenant\Credential;
+use App\Models\Tenant\DriveUpload;
 use Livewire\Component;
 
 class PendingCredentials extends Component
 {
-    public $showForm, $user_id, $credential_id,$documents;
+    public $showForm, $user_id, $credential_id,$documents, $file;
     protected $listeners = ['showList' => 'resetForm'];
 
     public function render()
@@ -24,6 +25,17 @@ class PendingCredentials extends Component
         }
     }
 
+    // public function upload(){
+    //     if ($this->drive_file != null) {
+    //         $fileService = new UploadFileService();
+    //         $this->field['document_path'] = $fileService->saveFile('drive/' . $this->field['record_type'] . '/' . $this->field['record_id'], $this->drive_file);
+    //     }
+    //     if ($this->field['expiration_date'] != null) //convert before saving
+    //     $this->field['expiration_date'] = Carbon::parse($this->field['expiration_date']);
+
+    //     DriveUpload::create($this->field);
+    //     $this->confirmation("File Uploaded to drive successfully");
+    // }
     function showForm()
     {     
        $this->showForm=true;
