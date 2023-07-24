3eddc3934eda8d8e1682f32c68e0397a6154ce20
Maarooshaa Asim <maarooshaa@gmail.com>
2023-07-24 17:55:41 +0500

    changing payment-preferences to centralized component

--- resources/views/tenant/provider/payment-preferences.blade.php
+++ resources/views/tenant/provider/payment-preferences.blade.php
@@ -1,5 +1,40 @@
 @extends('layouts.tenant', ['title' => 'Payment Preferences'])
 
 @section('content')
+<div>
+    <div class="content-header row">
+		<div class="content-header-left col-12 mb-3">
+			<div class="row breadcrumbs-top">
+				<div class="col-12">
+					<h1 class="content-header-title float-start mb-0">
+						Payment Preferences
+					</h1>
+					<div class="breadcrumb-wrapper">
+						<ol class="breadcrumb">
+							<li class="breadcrumb-item">
+								<a href="javascript:void(0)" title="Go to Dashboard" aria-label="Go to Dashboard">
+									<svg aria-label="Home" width="22" height="23" viewBox="0 0 22 23">
+                                        <use xlink:href="/css/common-icons.svg#home"></use>
+                                      </svg>
+								</a>
+							</li>
+							<li class="breadcrumb-item">
+								Settings
+							</li>
+							<li class="breadcrumb-item">
+								Payment Preferences
+							</li>
+						</ol>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+   <div class="card">
+    <div class="card-body">
   @livewire('app.provider.payment-preferences')
+   </div>
+   </div>
+   @include('modals.common.add-address')
+</div>
 @endsection
