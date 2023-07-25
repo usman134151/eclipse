a5cf68a609b85434581be850575ae675ba68db3e
amar <amar@abmasoft.com>
2023-07-25 10:05:58 +0000

    Revert "add checks in notifications_tag seeders and add data to notifications_table"
    This reverts commit 0c61d5b7761158f76980d4b59b05d418108052eb

--- database/seeders/NotificationTagsSeeder.php
+++ database/seeders/NotificationTagsSeeder.php
@@ -65,7 +65,6 @@ class NotificationTagsSeeder extends Seeder
         foreach ($notification_tags as $tag) {
             $existingTag = DB::table('notification_tags')->where('name', $tag['name'])->first();
             if (!$existingTag) {
-                //check if data alreay present 
                 DB::table('notification_tags')->insert($tag);
             }
         }
