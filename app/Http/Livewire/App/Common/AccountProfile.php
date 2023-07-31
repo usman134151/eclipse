6565b92a2bb0ef7c12bb6a7ddea576db51331c8d
Amna Bilal <amnausman@gmail.com>
2023-08-01 01:37:20 +0500

    User profile

--- app/Http/Livewire/App/Common/AccountProfile.php
+++ app/Http/Livewire/App/Common/AccountProfile.php
@@ -142,7 +142,7 @@ class AccountProfile extends Component
     public function showForm(User $user)
 {
     $this->user = $user;
-    $this->userdetail = $user->userdetail ? $user->userdetail->toArray() : ['user_position'=>'','address_line1'=>'','address_line2'=>'','zip'=>'','city'=>'','state'=>'','title'=>'','phone'=>'','ethnicity_id'=>'','gender_id'=>'','country'=>'0','profile_pic'=>''];
+    $this->userdetail = $user->userdetail ? $user->userdetail->toArray() : ['user_position'=>'','address_line1'=>'','address_line2'=>'','zip'=>'','city'=>'','state'=>'','title'=>'','phone'=>'','ethnicity_id'=>'0','gender_id'=>'0','country'=>'0','profile_pic'=>''];
     $this->showForm = true;
 }
     public function edit(User $user){
