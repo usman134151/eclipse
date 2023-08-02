bed80888535531f5211158d19ccf775ae8cddd4a
Gohar Shahzad <gohar@mindlogicsinc.com>
2023-08-02 22:12:35 +0500

    Business setup points 3, 4 completed

---
+++ app/Http/Livewire/App/Common/DashboardMessages.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Livewire\App\Common;
+
+use App\Models\Tenant\AnnouncementMessage;
+use Illuminate\Support\Carbon;
+use Livewire\Component;
+
+class DashboardMessages extends Component
+{
+    public $userType;
+    public $displayTo;
+    public $messages;
+
+    public function mount(){
+        $messages=[];
+        
+        if($this->displayTo=="login_screen"){
+            $messages=AnnouncementMessage::where('on_log_in_screen',1)->get()->toArray();
+        }else if($this->userType==1){
+            $messages=AnnouncementMessage::where('on_dashboard',1)->where('display_to_admin',1)->get()->toArray();
+        }else if($this->userType==2){
+            $messages=AnnouncementMessage::where('on_dashboard',1)->where('display_to_customers',1)->get()->toArray();
+        }else if($this->userType==3){
+            $messages=AnnouncementMessage::where('on_dashboard',1)->where('display_to_providers',1)->get()->toArray();
+        }
+        $messagesNoExpired=[];
+        foreach($messages as $messagee){
+            if($messagee['days']){
+                $expiry_date=Carbon::parse($messagee['days']);
+                if(!$expiry_date->isPast()){
+                    $messagesNoExpired[]=$messagee;
+                }
+            }else{
+                $messagesNoExpired[]=$messagee;
+            }
+        }
+        $this->messages=$messagesNoExpired;
+    }
+    public function render()
+    {
+        return view('livewire.app.common.dashboard-messages');
+    }
+
+}
