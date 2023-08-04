49dd11685257530733d5568b01439246cae0d9ed
Maarooshaa Asim <maarooshaa@gmail.com>
2023-08-04 15:53:19 +0500

    enabled time-off

--- app/Http/Livewire/App/Common/Panels/TimeOffSlots.php
+++ app/Http/Livewire/App/Common/Panels/TimeOffSlots.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Livewire\App\Common\Panels;
 
+use App\Models\Tenant\ProviderVacation;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class TimeOffSlots extends Component
 {
-    public $showForm,$provider_id;
-    protected $listeners = ['showList' => 'resetForm'];
+    public $showForm,$provider_id,$slot=[];
+    protected $listeners = ['showList' => 'resetForm', 'saveTimeOff','updateVal','clear'];
 
     public function render()
     {
@@ -16,8 +18,55 @@ class TimeOffSlots extends Component
 
     public function mount()
     {
+       $this->slot= ['from_date' => '', 'to_date' => '', 'user_id' => $this->provider_id, 'notes' => ''];
        
+    }
+
+    public function updateVal($attrName, $val)
+    {
+        $this->slot[$attrName] = $val;
+    }
+  
+
+    public function rules()
+    {
+        return [
+            'slot.from_date' => [
+                'date',
+                'required',
+                'date_format:m/d/Y', 'after:today'
+            ],
+
+            'slot.to_date' => [
+                'date',
+                'required',
+                'date_format:m/d/Y', 'after:slot.from_date'
+            ],
+            // 'timeslot.from_time' => [
+            //     'required',],
+            // 'timeslot.to_time' => [
+            //     'required',],
+        ];
+    }
+
+    public function saveTimeOff(){
+        $this->validate();
+        $this->slot['from_date']= Carbon::parse($this->slot['from_date']);
+        $this->slot['to_date'] = Carbon::parse($this->slot['to_date']);
+
+        ProviderVacation::create($this->slot);
+
+
+        $this->dispatchBrowserEvent('close-timeoff-panel');
+        $this->emit('showConfirmation', ' Time off dates saved!');
        
+
+    }
+
+    public function clear()
+    {
+        $this->slot = ['from_date' => '', 'to_date' => '', 'user_id' => $this->provider_id, 'notes' => ''];
+
     }
 
     function showForm()
--- resources/views/livewire/app/common/panels/time-off-slots.blade.php
+++ resources/views/livewire/app/common/panels/time-off-slots.blade.php
@@ -10,11 +10,11 @@
                                   Select Start Date 
                                 </label>
                                 <div class="position-relative">
-                                  <input type="" name="scheduled_date" class="form-control js-single-date" placeholder="MM/DD/YYYY" id="scheduled_date" wire:model.defer="timeslot.scheduled_date">
+                                  <input type="" name="scheduled_date" class="form-control js-single-date" placeholder="MM/DD/YYYY" id="from_date" wire:model.defer="slot.from_date">
                                   <svg aria-label="Date" class="icon-date" width="20" height="20" viewBox="0 0 20 20" fill="none"
                                                               xmlns="http://www.w3.org/2000/svg"><use xlink:href="/css/provider.svg#date-field"></use>
                                                           </svg>
-                                    @error('timeslot.scheduled_date')
+                                    @error('slot.from_date')
                                     <span class="d-inline-block invalid-feedback mt-2">
                                       {{ $message }}
                                     </span>
@@ -29,11 +29,11 @@
                                   Select End Date 
                                 </label>
                                 <div class="position-relative">
-                                  <input type="" name="scheduled_date" class="form-control js-single-date" placeholder="MM/DD/YYYY" id="scheduled_date" wire:model.defer="timeslot.scheduled_date">
+                                  <input type="" name="scheduled_date" class="form-control js-single-date" placeholder="MM/DD/YYYY" id="to_date" wire:model.defer="slot.to_date">
                                   <svg aria-label="Date" class="icon-date" width="20" height="20" viewBox="0 0 20 20" fill="none"
                                                               xmlns="http://www.w3.org/2000/svg"><use xlink:href="/css/provider.svg#date-field"></use>
                                                           </svg>
-                                    @error('timeslot.scheduled_date')
+                                    @error('slot.to_date')
                                     <span class="d-inline-block invalid-feedback mt-2">
                                       {{ $message }}
                                     </span>
@@ -50,18 +50,18 @@
                 <label class="form-label" for="notes">
                     Note
                 </label>
-                <textarea class="form-control" rows="4"  wire:model.defer="timeslot.notes" id="notes"></textarea>
+                <textarea class="form-control" rows="4"  wire:model.defer="slot.notes" id="notes"></textarea>
             </div>
-            <div>
+            {{-- <div>
                 <p>No Bookings Assigned from 11/02/2022 to 11/02/2022</p>
-            </div>
+            </div> --}}
         </div>
       <div class="col-12 justify-content-center form-actions d-flex gap-3">
           <button type="button" class="btn btn-outline-dark rounded" x-on:click="timeOffSlots = !timeOffSlots">
               Cancel
           </button>
           {{--  --}}
-          <button type="submit" class="btn btn-primary rounded" wire:click.prevent="$emit('saveSpecificSlot')" x-on:close-specific-panel.window="timeOffSlots = !timeOffSlots">
+          <button type="submit" class="btn btn-primary rounded" wire:click.prevent="$emit('saveTimeOff')" x-on:close-timeoff-panel.window="timeOffSlots = !timeOffSlots">
               Save
           </button>
       </div>
