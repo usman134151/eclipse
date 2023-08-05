09a8eb7b84f383ff5aaf81283855885e20ab7e32
Maarooshaa Asim <maarooshaa@gmail.com>
2023-08-05 15:35:23 +0500

    schedule enabled in department-profile->settings

--- app/Http/Livewire/App/Common/DepartmentProfile.php
+++ app/Http/Livewire/App/Common/DepartmentProfile.php
@@ -9,7 +9,7 @@ use Livewire\Component;
 class DepartmentProfile extends Component
 {
 	public $showForm;
-	public $department;
+	public $department, $schedule;
 	protected $listeners = [
 		'showDepartmentDetails', 'showConfirmation'
 	];
@@ -64,8 +64,41 @@ class DepartmentProfile extends Component
 			$days = ["Monday" => 1, "Tuesday" => 2, "Wednesday" => 3, "Thursday" => 4, "Friday" => 5, "Saturday" => 6, "Sunday" => 7];
 
 			$this->department['schedule'] = $schedule->timeslots->groupBy('timeslot_day')->sortBy(fn ($val, $key) => $days[$key])->toArray();}
+
+		$this->getDepartmentSchedule();
 		
         $this->dispatchBrowserEvent('refreshSelects');
 
 	}
+
+	public function getDepartmentSchedule()
+	{
+		//reinit schedule
+		$departmentSchedule = Schedule::where('model_id', $this->department->id)->where('model_type', 4)->get()->first();
+		if (!is_null($departmentSchedule)) {
+			$this->schedule = $departmentSchedule;
+		} else {
+			$this->schedule = new Schedule;
+			$this->schedule->model_type = 4;
+			$this->schedule->working_days = json_encode([]);
+			$this->schedule->timezone_id = 0;
+
+			$this->schedule->model_id = $this->department->id;
+			$this->schedule->save();
+		}
+		//as this function is called from mount, cant be emitted from here. 
+		//emitted when settings tab is clicked.
+		// $this->emit('getRecord', $this->schedule->id, true);
+	}
+
+	public function saveSchedule($redirect = 1)
+	{
+		//save other fields as well
+		$this->emit('saveSchedule');
+		$this->showConfirmation("Department has been saved successfully");
+		
+	}
+
+
+
 }
