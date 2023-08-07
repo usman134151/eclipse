9699555257c39af2e470dacefa3bd82814d61e55
Maarooshaa Asim <maarooshaa@gmail.com>
2023-08-07 14:21:14 +0000

    booking-list initial display

--- app/Http/Livewire/App/Common/Bookings/BookingList.php
+++ app/Http/Livewire/App/Common/Bookings/BookingList.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\App\Common\Bookings;
 
+use App\Models\Tenant\Booking;
 use Livewire\Component;
 
 class BookingList extends Component
@@ -9,17 +10,24 @@ class BookingList extends Component
 	public $bookingType;
 	public $showBookingDetails;
 	public $bookingSection;
+	public $booking_assignments, $limit = 10;
+	
 
-	protected $listeners = ['showList' => 'resetForm'];
+protected $listeners = ['showList' => 'resetForm', 'updateVal'];
 
 	public function render()
 	{
+		$this->booking_assignments = Booking::where('company_id','<>',null)->limit($this->limit)->with(['company'])->get();
 		return view('livewire.app.common.bookings.booking-list');
 	}
 
 	public function mount()
 	{
 		
+	}
+
+	public function updateVal($attrName,$val){
+		$this->$attrName = $val;
 	}
 
 	public function resetForm()
