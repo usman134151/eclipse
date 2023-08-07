7155556061d22ec1cfcf0d54a6811426cf2e19ef
Gohar Shahzad <gohar@mindlogicsinc.com>
2023-08-07 21:36:15 +0500

    fixed error in advance provider filters 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        $this->app->bind('advance-provider-filters', AdvanceProviderFilters::class);
     }
 
     /**
