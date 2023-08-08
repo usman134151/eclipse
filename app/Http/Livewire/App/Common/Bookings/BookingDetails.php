982986f9cbfc8b003041d0d6480f3a6b1a770420
Maarooshaa Asim <maarooshaa@gmail.com>
2023-08-08 17:34:04 +0500

    display and delete booking-attachments enabled

--- app/Http/Livewire/App/Common/Bookings/BookingDetails.php
+++ app/Http/Livewire/App/Common/Bookings/BookingDetails.php
@@ -10,6 +10,8 @@ class BookingDetails extends Component
     public $gender;
     public $ethnicity, $booking_id=0;
 	public $component = 'booking-details';
+	protected $listeners = ['showConfirmation'];
+
 
 	public function render()
 	{	
@@ -25,6 +27,17 @@ class BookingDetails extends Component
 	public function mount()
 	{}
 
+	public function showConfirmation($message = '')
+	{
+		if ($message) {
+			// Emit an event to display a success message using the SweetAlert package
+			$this->dispatchBrowserEvent('swal:modal', [
+				'type' => 'success',
+				'title' => 'Success',
+				'text' => $message,
+			]);
+		}
+	}
 	public function showList()
 	{
 		$this->emit('showList');
