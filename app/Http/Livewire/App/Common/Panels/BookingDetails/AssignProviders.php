c80a4267e8cea242f2e73d57275bdc6bc3967605
Gohar Shahzad <gohar@mindlogicsinc.com>
2023-08-09 23:52:20 +0500

    Assign Providers filter continue

--- app/Http/Livewire/App/Common/Panels/BookingDetails/AssignProviders.php
+++ app/Http/Livewire/App/Common/Panels/BookingDetails/AssignProviders.php
@@ -2,27 +2,41 @@
 
 namespace App\Http\Livewire\App\Common\Panels\BookingDetails;
 
+use App\Models\Tenant\Tag;
 use App\Models\Tenant\User;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class AssignProviders extends Component
 {
+	use WithPagination;
+
     public $showForm;
-    public $providers;
+    public $allproviders;
+    public $tags;
     protected $listeners = ['showList' => 'resetForm'];
 
     public function render()
     {
-        return view('livewire.app.common.panels.booking-details.assign-providers');
+        // dd($this->providers);
+        $providers=User::where('status',1)
+                ->whereHas('roles', function ($query) {
+                    $query->wherein('role_id',[2]);
+                })->get();
+        return view('livewire.app.common.panels.booking-details.assign-providers',[
+            'providers'=>$providers
+        ]);
     }
 
     public function mount()
     {
        
-       $this->providers=User::where('status',1)
+       $this->allproviders=User::where('status',1)
                             ->whereHas('roles', function ($query) {
                                $query->wherein('role_id',[2]);
                            })->get();
+        $this->tags=Tag::all();
+
        
     }
 
