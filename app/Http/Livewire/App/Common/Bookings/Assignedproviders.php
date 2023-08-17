bef5a900e15456edfab7a913eddc4279ebde053c
khadeeja Kasmani <khadijahnaeem2000@gmail.com>
2023-08-17 22:21:45 +0500

    booking details task updated

---
+++ app/Http/Livewire/App/Common/Bookings/Assignedproviders.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Livewire\App\Common\Bookings;
+
+use Livewire\Component;
+use App\Models\Tenant\BookingProvider;
+use App\Models\Tenant\User;
+use Livewire\WithPagination;
+
+class Assignedproviders extends Component
+{
+    use WithPagination;
+    public $booking_id;
+    public $limit;
+
+    protected $assignedProviders;
+
+    public function mount($booking_id)
+    {
+        $this->booking_id = $booking_id;
+        $this->loadAssignedProviders();
+    }
+
+    public function render()
+    {
+        return view('livewire.app.common.bookings.assignedproviders', ['assignedProviders' => $this->assignedProviders]);
+    }
+
+    private function loadAssignedProviders()
+    {
+        $this->assignedProviders = BookingProvider::where('booking_id', $this->booking_id)
+            ->join('users', 'booking_providers.provider_id', '=', 'users.id')
+            ->paginate(5);
+    }
+    
+}
+
+
