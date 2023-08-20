053c9f9530fad953f1085c8859dfb7a9e3251ad5
Maarooshaa Asim <maarooshaa@gmail.com>
2023-08-20 18:50:02 +0500

    attach service id on panel - assign providers

--- resources/views/panels/booking-details/assign-providers.blade.php
+++ resources/views/panels/booking-details/assign-providers.blade.php
@@ -1,6 +1,6 @@
 {{-- BEGIN: Assign Provider Off Canvas --}}
 <x-off-canvas show="assignProvider" size="fullscreen">
-    <x-slot name="title">Assign Provider {{$currentServiceId}}</x-slot>
+    <x-slot name="title">Assign Provider</x-slot>
     @if($currentServiceId>0)
         @livewire('app.common.panels.booking-details.assign-providers',['service_id'=>$currentServiceId,'booking_id'=> $booking_id])
     @endif
