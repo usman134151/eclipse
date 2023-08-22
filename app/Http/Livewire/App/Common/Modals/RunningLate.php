4713a2016a5857761c6fa89b9553848663b90f5b
Maarooshaa Asim <maarooshaa@gmail.com>
2023-08-22 18:47:53 +0500

    running late modal enabled

--- app/Http/Livewire/App/Common/Modals/RunningLate.php
+++ app/Http/Livewire/App/Common/Modals/RunningLate.php
@@ -3,24 +3,28 @@
 namespace App\Http\Livewire\App\Common\Modals;
 
 use App\Models\Tenant\Booking;
+use App\Models\Tenant\BookingProvider;
 use App\Models\Tenant\ServiceCategory;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class RunningLate extends Component
 {
-    public $showForm , $service , $hours,$mins, $customSet=false;
+    public $booking_id, $showForm, $service, $hours, $mins, $customSet = false;
     protected $listeners = ['showList' => 'resetForm', 'openRunningLateModal' => 'setDetails'];
 
-    public function setDetails($booking_id, $service_id=null)
+    public function setDetails($booking_id, $service_id = null)
     {
+        $this->hours = null;
+        $this->mins = null;
 
+        $this->booking_id = $booking_id;
         if (is_null($service_id)) {
             //if booking_providers.service_id not set, fetch first service from booking_services 
-            $booking = Booking::where('id',$booking_id)->first();
-            $this->service = $booking->services->first();  
-        }else
-        $this->service = ServiceCategory::where('id',$service_id)->first();
-        // dd($this->service);
+            $booking = Booking::where('id', $booking_id)->first();
+            $this->service = $booking->services->first();
+        } else
+            $this->service = ServiceCategory::where('id', $service_id)->first();
     }
 
     public function render()
@@ -28,21 +32,38 @@ class RunningLate extends Component
         return view('livewire.app.common.modals.running-late');
     }
 
-    public function fastSet($mins){
-        // $this->customSet=false;
-        $this->hours =00;
+    public function fastSet($mins)
+    {
+        $this->hours = 00;
         $this->mins = $mins;
+    }
+    public function rules()
+    {
+        return [
+            'hours' => 'nullable|numeric|gt:0',
+            'mins' => 'nullable|numeric|gt:0',
+
+        ];
     }
 
     // booking_providers => check_in_status  (0= on time, 1= checked in, 2= running late)
-    public function save(){
+    public function save()
+    {
+        $this->validate();
+        if ($this->hours > 0 || $this->mins > 0) {
+            $bookingProviderService = BookingProvider::where(['booking_id' => $this->booking_id, 'provider_id' => Auth::id()])->first();
+            $bookingProviderService->running_late_hour = $this->hours;
+            $bookingProviderService->running_late_min = $this->mins;
+            $bookingProviderService->check_in_status = 2;
+            $bookingProviderService->save();
+        }
         $this->emit('closeRunningLateModal');
     }
 
     public function mount()
     {
-        $this->hours=00;
-        $this->mins=00;
+        $this->hours = null;
+        $this->mins = null;
     }
 
     function showForm()
--- resources/views/livewire/app/common/modals/running-late.blade.php
+++ resources/views/livewire/app/common/modals/running-late.blade.php
@@ -1,33 +1,47 @@
 <div class="modal-content">
     <div class="modal-header">
-      <h2 class="modal-title fs-5" id="runningLateModalLabel">Running Late</h2>
-      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <h2 class="modal-title fs-5" id="runningLateModalLabel">Running Late</h2>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
     </div>
     <div class="modal-body">
-      <div class="d-flex justify-content-center flex-column align-items-center">
-        <p class="mb-4">How much time running late for “{{isset($service->name) ? $service->name : ''}}”</p>
-        <div class="d-flex gap-3 align-items-center mb-4">
-          <a href="#" wire:click="fastSet(15)" class="btn btn-outline-dark btn-sm rounded">15 mins</a>
-          <a href="#" wire:click="fastSet(30)" class="btn btn-outline-dark btn-sm rounded">30 mins</a>
-          <a href="#" wire:click="fastSet(45)" class="btn btn-outline-dark btn-sm rounded">45 mins</a>
-          <a href="#"  class="btn btn-outline-dark btn-sm rounded">Custom</a>
-        </div>
-        <div class="row justify-content-center ">
-          <div class="col-lg-5">
-            <div class="d-flex justify-content-around">
-              <label class="form-label-sm" aria-label="Hours">Hours</label>
-              <label class="form-label-sm" aria-label="Minutes">Minutes</label>
+        <div class="d-flex justify-content-center flex-column align-items-center">
+            <p class="mb-4">How much time running late for “{{ isset($service->name) ? $service->name : '' }}”</p>
+            <div class="d-flex gap-3 align-items-center mb-4">
+                <a href="#" wire:click="fastSet(15)" class="btn btn-outline-dark btn-sm rounded">15 mins</a>
+                <a href="#" wire:click="fastSet(30)" class="btn btn-outline-dark btn-sm rounded">30 mins</a>
+                <a href="#" wire:click="fastSet(45)" class="btn btn-outline-dark btn-sm rounded">45 mins</a>
+                <a href="#" class="btn btn-outline-dark btn-sm rounded">Custom</a>
             </div>
-            <div class="input-group">
-              <input type="number" wire:model.defer="hours" class="form-control form-control-md text-center" value="00" aria-label="Hours">
-              <input type="number" wire:model.defer="mins"  class="form-control form-control-md text-center" value="00" aria-label="Hours">
+            <div class="row justify-content-center ">
+                <div class="col-lg-5">
+                    <div class="d-flex justify-content-around">
+                        <label class="form-label-sm" aria-label="Hours">Hours</label>
+                        <label class="form-label-sm" aria-label="Minutes">Minutes</label>
+                    </div>
+                    <div class="input-group">
+                        <input type="number" wire:model.defer="hours" class="form-control form-control-md text-center"
+                            placeholder="00" aria-label="Hours">
+                        <input type="number" wire:model.defer="mins" class="form-control form-control-md text-center"
+                            placeholder="00" aria-label="Hours">
+                    </div>
+                </div>
+                <div class="text-center">
+                @error('hours')
+                    <span class="d-inline-block invalid-feedback mt-2">
+                        {{ $message }}
+                    </span>
+                @enderror
+                @error('mins')
+                    <span class="d-inline-block invalid-feedback mt-2">
+                        {{ $message }}
+                    </span>
+                @enderror
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
     <div class="modal-footer d-flex justify-content-center gap-2">
-      <button type="button" class="btn rounded btn-outline-dark" data-bs-dismiss="modal">Cancel</button>
-      <button type="button" wire:click="save" class="btn rounded btn-primary" >Notify</button>
+        <button type="button" class="btn rounded btn-outline-dark" data-bs-dismiss="modal">Cancel</button>
+        <button type="button" wire:click="save" class="btn rounded btn-primary">Notify</button>
     </div>
-  </div>
+</div>
