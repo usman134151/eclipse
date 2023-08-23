6718c81e44629e1ee8c1e12d5e4add5cf9731933
khadeeja Kasmani <khadijahnaeem2000@gmail.com>
2023-08-23 17:20:54 +0500

    map filters

--- app/Http/Livewire/App/Common/Map.php
+++ app/Http/Livewire/App/Common/Map.php
@@ -6,14 +6,20 @@ use Livewire\Component;
 use App\Models\Tenant\User;
 use App\Models\Tenant\Booking;
 use App\Models\Tenant\UserAddress;
+use DB;
 
 class Map extends Component
 {
     public $showForm;
-    protected $listeners = ['showList' => 'resetForm'];
+    protected $listeners = ['showList' => 'resetForm','apply'=> 'applyFilters'];
     public $locations=[];
+    public $selectedDate;
+    public $selectedBookingNo;
+    public $selectedAddress;
+    public $bookingList;
+    public $addressList;
+    public $selectedBooking;
   
-
 
     public function render()
     {
@@ -24,41 +30,95 @@ class Map extends Component
 
     public function mount()
     {
-        $locations = Booking::
-        join('user_addresses', 'bookings.physical_address_id', '=', 'user_addresses.id')
-        ->join('booking_services', 'bookings.id', '=', 'booking_id')
-        ->join('service_categories', 'booking_services.services', '=', 'service_categories.id')
-        ->select([
-            'bookings.booking_number',
-            'service_categories.name as service_name',
-            'user_addresses.address_name',
-            'user_addresses.address_line1',
-            'user_addresses.latitude',
-            'user_addresses.longitude',
-            'user_addresses.city',
-            'user_addresses.state',
-            'user_addresses.country',
-            'user_addresses.zip',
-        ])->get();
-    
-        $locationsArray = [];
-    
-        foreach ($locations as $location) {
-            $locationData = [
-                'title' => $location->booking_number,
-                'service' => $location->service_name,
-                'address' => $location->address_line1 . ', ' . $location->city . ', ' . $location->state . ', ' . $location->country . ', ' . $location->zip,
-                'lat' => $location->latitude,
-                'long' => $location->longitude
-            ];
-    
-            $locationsArray[] = $locationData;
-        }
-    
-        $this->locations = $locationsArray;
+        $this->bookingList=Booking::select('id','booking_number')->get();
+         $this->addressList = UserAddress::select([
+            DB::raw("CONCAT(address_line1,', ', city, ', ', state, ', ', country) as full_address")
+            ])->get();
+      
+           $this->applyFilters();
     }
+
+   // seprate function for implementing the filters
+    public function applyFilters()
+    {
+       
+        $locations = Booking::join('user_addresses', 'bookings.physical_address_id', '=', 'user_addresses.id')
+            ->join('booking_services', 'bookings.id', '=', 'booking_id')
+            ->join('service_categories', 'booking_services.services', '=', 'service_categories.id')
+            ->when($this->selectedDate, function ($query, $selectedDate) {
+                return $query->whereDate('bookings.booking_start_at', date("Y-m-d", strtotime($this->selectedDate)));
+            })
+            ->when($this->selectedAddress, function ($query, $selectedAddress) {
+                return $query->whereRaw("CONCAT(address_line1, ', ', city, ', ', state, ', ', country) LIKE ?", ['%' . $selectedAddress . '%']);
+
+            })
+            ->when($this->selectedBookingNo, function ($query, $selectedBookingNo) {
+                return $query->where('bookings.id',$selectedBookingNo);
+            })
+            ->select([
+                'bookings.booking_number',
+                'service_categories.name as service_name',
+                'user_addresses.address_name',
+                'user_addresses.address_line1',
+                'user_addresses.latitude',
+                'user_addresses.longitude',
+                'user_addresses.city',
+                'user_addresses.state',
+                'user_addresses.country',
+                'user_addresses.zip',
+            ])->get();
+
+            $locationsArray = [];
+    
+            foreach ($locations as $location) {
+                $locationData = [
+                    'title' => $location->booking_number,
+                    'service' => $location->service_name,
+                    'address' => $location->address_line1 . ', ' . $location->city . ', ' . $location->state . ', ' . $location->country . ', ' . $location->zip,
+                    'lat' => $location->latitude,
+                    'long' => $location->longitude
+                ];
+        
+                $locationsArray[] = $locationData;
+            }
+        
+            $this->locations = $locationsArray;
+           
+            $this->dispatchBrowserEvent('livewire:map',[
+                'locations'=>$this->locations
+                 ]);
+          
     
 
+    }
+    //this is for filters
+    public function updateVal($inputId, $inputValue)
+    {
+        switch ($inputId) {
+            case "Booking":
+
+                $selectedBooking=Booking::where('id',$inputValue)->select('Booking_number')->first();
+                $this->selectedBooking=$selectedBooking->Booking_number;
+                $this->selectedBookingNo=$inputValue;
+
+                break;
+            
+            case "Address":
+                 $this->selectedAddress=$inputValue;
+                break;
+    
+            case "selecteddate":
+                $this->selectedDate = $inputValue;
+               
+                break;
+        }
+        // Trigger the filter action after updating the property
+        $this->applyFilters();
+        $this->addressList = UserAddress::select([
+            DB::raw("CONCAT(address_line1,', ', city, ', ', state, ', ', country) as full_address")
+            ])->get();
+    }
+    
     function showForm()
     {     
        $this->showForm=true;
@@ -66,6 +126,15 @@ class Map extends Component
     public function resetForm()
     {
         $this->showForm=false;
+    }
+    public function resetDate()
+    {
+        $this->selectedBooking = null; // Reset the selected date and bookingid etc
+        $this->selectedAddress=null;
+        $this->selectedDate =null;
+        $this->selectedBookingNo=null;
+        $this->applyFilters();
+      
     }
 
 }
--- resources/views/livewire/app/common/map.blade.php
+++ resources/views/livewire/app/common/map.blade.php
@@ -13,23 +13,48 @@
 </style>
 <h3>Map</h3>
 		<!-- Filters -->
+
+    <div class="col-sm">
+    <button class="btn btn-secondary btn-sm reset-button" wire:click="resetDate">Reset</button>
+    </div>
 		<div class="row mb-4">
 		  <div class="col-lg-3 mb-4 mb-lg-0 position-relative align-self-end">
 			<!-- Begin : it will be replaced with livewire module-->
 			<svg aria-label="Date" class="icon-date md cursor-pointer" width="20" height="20" viewBox="0 0 20 20" fill="none"
                xmlns="http://www.w3.org/2000/svg"><use xlink:href="/css/provider.svg#date-field"></use>
             </svg>
-			<input type="" class="form-control form-control-md form-control-date js-single-date" placeholder="MM/DD/YYYY" name="selectDate" aria-label="Select Date">
+            <input type="" class="form-control form-control-md form-control-date js-single-date" placeholder="MM/DD/YYYY" name="selectDate" aria-label="Select Date" id="selecteddate">
+			<!-- End : it will be replaced with livewire module -->
+
 			<!-- End : it will be replaced with livewire module -->
 		  </div>
-		  <div class="col-lg mb-4 mb-lg-0">
-			<label class="form-label-sm" for="address">Address</label>
-			<input type="" name="" class="form-control form-control-md" placeholder="Search" id="address">
-		  </div>
-		  <div class="col-lg mb-4 mb-lg-0">
-			<label class="form-label-sm" for="booking_no">Booking No</label>
-			<input type="" name="" class="form-control form-control-md" placeholder="Search" id="booking_no">
-		  </div>
+      <div class="col-lg mb-4 mb-lg-0">
+            <label class="form-label" for="supervisor">Address</label>
+            <select class="form-select select2 address">
+            @if(!empty($selectedAddress))
+            <option>{{$selectedAddress}}</option>
+            @else
+            <option value="0">Address</option>
+            @endif
+            @foreach($addressList as $address)
+                    <option value="{{$address->full_address }}">{{$address->full_address}}</option>   
+                @endforeach
+            </select>
+        </div>
+      <div class="col-lg mb-4 mb-lg-0">
+            <label class="form-label" for="supervisor">Bookings</label>
+            <select class="form-select select2 booking" id="BookingID" name="BookingID"  wire:click="ChangeFilter($event.target.value,'Booking')">
+            @if(!empty($selectedBooking))
+            <option>{{$selectedBooking}}</option>
+            @else
+            <option value="0">Select Booking</option>
+            @endif
+              
+                @foreach($bookingList as $list)
+                    <option value="{{$list->id}}">{{$list->booking_number}}</option>   
+                @endforeach
+            </select>
+        </div>
 		  <div class="col-lg mb-4 mb-lg-0">
 			<div class="d-flex flex-column -mt-5">
 			  <div class="form-check">
@@ -53,20 +78,22 @@
 			</div>
 		  </div>
 		</div>
+    
 		<!-- /Filters -->
-		<div id="map"></div>
+		<div id="map"  wire:ignore></div>
 
 </div>
 
-@push('scripts')
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
-        <script type="text/javascript">
-            document.addEventListener("livewire:load", function() {
+<script type="text/javascript"> 
+
+       
+               document.addEventListener('livewire:load', function () {
+               
                 var locations = @json($locations);
+               
                 var map = new google.maps.Map(document.getElementById("map"), {
-                    zoom:3,
+                    zoom: 3,
                     center: { lat: 37.7749, lng: -122.4194 }, // Set a default center
-                 
                 });
 
                 var geocoder = new google.maps.Geocoder();
@@ -77,9 +104,35 @@
                     } else {
                         geocodeAndCreateMarker(geocoder, map, locations[i]);
                     }
-                }
+                } 
             });
+            document.addEventListener('livewire:map', function (e) {
+              
+              var locations = e.detail.locations;
+              var center;
 
+              if (locations.length === 0) {
+                  center = { lat: 37.7749, lng: -122.4194 };
+              } else {
+                  center = { lat: 0, lng: 0 };
+              }
+
+            
+               var map = new google.maps.Map(document.getElementById("map"), {
+                   zoom: 2,
+                   center:center, // Set a default center
+               });
+
+               var geocoder = new google.maps.Geocoder();
+
+               for (var i = 0; i < locations.length; i++) {
+                   if (locations[i].lat && locations[i].long) {
+                       createMarkerWithDetail(map, locations[i]);
+                   } else {
+                       geocodeAndCreateMarker(geocoder, map, locations[i]);
+                   }
+               }
+           });
             function createMarkerWithDetail(map, location) {
                 var latLng = new google.maps.LatLng(location.lat, location.long);
 
@@ -119,9 +172,8 @@
                             }
                         });
                         
-
                        // var content = '<a href="https://www.google.com/maps/place/'+location.address+'">View on Google Maps</a>';
-					   var content = '<div class="marker-label"><p><strong>Assignment Number: ' + location.title + '</strong></p><p>' + location.service + '</p><p>Address: ' + location.address + '</p><a href="https://www.google.com/maps/place/' + encodeURIComponent(location.address)+'" target="_blank">Get Directions</a>&nbsp;&nbsp;&nbsp; <a  style="float:right;"href="#">Booking Details</a></div>';
+					          var content = '<div class="marker-label"><p><strong>Assignment Number: ' + location.title + '</strong></p><p>' + location.service + '</p><p>Address: ' + location.address + '</p><a href="https://www.google.com/maps/place/' + encodeURIComponent(location.address)+'" target="_blank">Get Directions</a>&nbsp;&nbsp;&nbsp; <a  style="float:right;"href="#">Booking Details</a></div>';
 
 
                         var infoWindow = new google.maps.InfoWindow({
@@ -137,10 +189,22 @@
                  
 
                 });
+
+                $('.booking').on('change', function (e) {
+                   //@this.(e.target.value,'Booking');
+                    @this.call('updateVal','Booking', e.target.value); // Trigger the Livewire method
+                });
+                $('.address').on('change', function (e) {
+                   //@this.(e.target.value,'Booking');
+                    @this.call('updateVal','Address', e.target.value); // Trigger the Livewire method
+                });
+              $('.js-single-date').on('apply.daterangepicker', function(ev, picker) {
+                        const inputId = $(this).attr('id');
+                        const inputValue = $(this).val();
+                        // Call the Livewire method to update the value
+                        @this.call('updateVal', inputId, inputValue);
+                    });
             }
-        </script>
-        <script type="text/javascript"
-            src="https://maps.google.com/maps/api/js?key=AIzaSyAANwmAq3UQc8j5GkJgzF9AglzF7XLfPxI&libraries"></script>
-    @endpush
+        </script> 
 
 
