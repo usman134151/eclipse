e57a83b6e1044c771f0d870d8ea74719036fe7c0
Maarooshaa Asim <maarooshaa@gmail.com>
2023-08-23 11:20:23 +0500

    migration for booking_service_id

---
+++ database/migrations/tenant/2023_08_23_061630_add_booking_services_id_in_booking_providers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('booking_providers', function (Blueprint $table) {
+            //
+            $table->dropColumn('service_id');
+            $table->unsignedBigInteger('booking_service_id')->nullable()->default(null);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('booking_providers', function (Blueprint $table) {
+            //
+        });
+    }
+};
