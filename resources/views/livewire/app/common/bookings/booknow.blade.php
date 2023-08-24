f426bbf8dd12166d7a1061a707e5974f6365f74a
Amna Bilal <amnausman@gmail.com>
2023-08-24 15:19:26 +0500

    removed temp text for beta

--- resources/views/livewire/app/common/bookings/booknow.blade.php
+++ resources/views/livewire/app/common/bookings/booknow.blade.php
@@ -913,7 +913,7 @@
                     @elseif($component == 'payment-info')
                     <div class="tab-pane fade {{ $component == 'payment-info' ? 'active show' : '' }}" id="payment-info"
                         role="tabpanel" aria-labelledby="payment-info-tab" tabindex="0">
-                        <table>{!! $bookingDetails['html'] !!}</table>
+                      
 
                             <h2>Payment Summary</h2>
                             <div class="row">
