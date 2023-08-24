101d2077fa15536f332d24184c83cf2614a204a5
Maarooshaa Asim <maarooshaa@gmail.com>
2023-08-24 07:28:57 +0000

    custom-form-display

---
+++ database/migrations/tenant/2023_08_24_065808_add_null_fields_in_booking_customize_data.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('booking_customize_data', function (Blueprint $table) {
+            $table->integer('booking_log_id')->nullable()->default(null)->change();
+            $table->text('field_title')->nullable()->default(null)->change();
+            $table->string('booking_log_bbid')->nullable()->default(null)->change();
+            $table->integer('quote_id')->nullable()->default(null)->change();
+            $table->integer('provider_application_id')->nullable()->default(null)->change();
+            $table->integer('booking_log_id')->nullable()->default(null)->change();
+            
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('booking_customize_data', function (Blueprint $table) {
+            //
+        });
+    }
+};
