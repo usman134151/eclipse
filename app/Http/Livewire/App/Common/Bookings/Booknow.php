d7b2526bea420146a951adbace0cd7c07a073d8d
Amna Bilal <amnausman@gmail.com>
2023-09-07 18:43:46 +0500

    Calculations revamp

--- app/Http/Livewire/App/Common/Bookings/Booknow.php
+++ app/Http/Livewire/App/Common/Bookings/Booknow.php
@@ -746,7 +746,7 @@ class Booknow extends Component
         }
       
         $this->services=BookingOperationsService::getBookingCharges($this->booking,$bookingServices,$this->dates);
-        dd($this->services);
+      //  dd($this->services);
        // dd($this->bookingCharges);
        // $this->bookingDetails=BookingOperationsService::getBookingInfoNewLayout($this->booking);
        
