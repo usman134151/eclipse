f238a91906e32a655b325067a316f10620b18817
Maarooshaa Asim <maarooshaa@gmail.com>
2023-09-09 17:14:55 +0500

    unassign modal linkage

--- app/Http/Livewire/App/Common/Modals/Unassign.php
+++ app/Http/Livewire/App/Common/Modals/Unassign.php
@@ -6,27 +6,45 @@ use Livewire\Component;
 
 class Unassign extends Component
 {
-    public $showForm;
-    protected $listeners = ['showList' => 'resetForm'];
+    public $showForm, $booking_id = null, $provider_id = null, $booking_service_id = null, $data=[];
+    protected $listeners = ['showList' => 'resetForm', 'openUnassignModal' => 'setBooking','updateVal'];
 
     public function render()
     {
         return view('livewire.app.common.modals.unassign');
     }
 
+    public function updateVal($attrName, $val){
+        $this->data[$attrName]= $val;
+    }
     public function mount()
     {
-       
-       
+        $this->data=[
+            'unassign_reason'=>'',
+            'unassign_date'=>null,
+            'hour'=>null,
+            'min'=>null,
+        ];
     }
 
+    public function setBooking($booking_service_id = null, $provider_id, $booking_id)
+    {
+        $this->dispatchBrowserEvent('refreshSelects');
+
+        $this->booking_id = $booking_id;
+        $this->booking_service_id = $booking_service_id;
+        $this->provider_id = $provider_id;
+    }
+
+    public function save(){
+        $this->emit('closeUnassignModel');
+    }
     function showForm()
-    {     
-       $this->showForm=true;
+    {
+        $this->showForm = true;
     }
     public function resetForm()
     {
-        $this->showForm=false;
+        $this->showForm = false;
     }
-
 }
