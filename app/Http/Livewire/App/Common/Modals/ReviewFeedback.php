8ede34fba803fa01211df1100661bfff228d2922
Maarooshaa Asim <maarooshaa@gmail.com>
2023-09-13 15:55:43 +0500

    save and display feedback from checkout

--- app/Http/Livewire/App/Common/Modals/ReviewFeedback.php
+++ app/Http/Livewire/App/Common/Modals/ReviewFeedback.php
@@ -7,32 +7,40 @@ use Livewire\Component;
 
 class ReviewFeedback extends Component
 {
-    public $showForm, $feedback=[];
-    protected $listeners = ['showList' => 'resetForm', 'openFeedBackModal'=>'loadFeedback'];
+    public $showForm, $feedback = [];
+    protected $listeners = ['showList' => 'resetForm', 'openFeedBackModal' => 'loadFeedback', 'openAllFeedBackModal'];
 
     public function render()
     {
         return view('livewire.app.common.modals.review-feedback');
     }
 
-    
-    public function  loadFeedback($booking_service_id,$provider_id)
+
+    public function  loadFeedback($booking_service_id, $provider_id)
     {
         $this->feedback = FeedbackRating::where([
             'feedback_from' => $provider_id,
             'booking_service_id' => $booking_service_id
-        ])->with(['to','from'])->get()->toArray();
-       
-       
+        ])->with(['to', 'from'])->get()->toArray();
+    }
+    public function openAllFeedBackModal($booking_id)
+    {
+
+        $this->feedback = FeedbackRating::whereIn(
+            'booking_service_id',function ($q) use ($booking_id) {
+                $q->from('booking_services')
+                    ->select('id')
+                    ->where('booking_id', $booking_id);
+            }
+        )->with(['to', 'from'])->get()->toArray();
     }
 
     function showForm()
-    {     
-       $this->showForm=true;
+    {
+        $this->showForm = true;
     }
     public function resetForm()
     {
-        $this->showForm=false;
+        $this->showForm = false;
     }
-
 }
