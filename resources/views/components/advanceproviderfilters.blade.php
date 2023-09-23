cbd213c4b2e2a441546a284c541942f2d160d5fb
Amna Bilal <amnausman@gmail.com>
2023-09-23 18:42:32 +0500

    debuggin advance filter toggle

--- resources/views/components/advanceproviderfilters.blade.php
+++ resources/views/components/advanceproviderfilters.blade.php
@@ -53,8 +53,8 @@
         <div class="col-lg-6 d-flex justify-content-start gap-2 mb-4">
             <div class="mb-4 mb-lg-0">
                 <button class="btn btn-xs btn-inactive dropdown-toggle bg-secondary rounded"
-                    data-bs-toggle="collapse" href="#collapseAdvanceFilter" role="button"
-                    aria-expanded="false" aria-controls="collapseAdvanceFilter">
+                    data-bs-toggle="collapse" href="#collapseAdvanceFilter{{$type}}" role="button"
+                    aria-expanded="false" aria-controls="collapseAdvanceFilter{{$type}}">
                     Advance Filter
                 </button>
             </div>
@@ -66,14 +66,14 @@
         </div>
     @else
     <div class="col-lg-2 d-flex text-nowrap align-items-center align-self-end mb-4">
-        <a class="btn btn-inactive dropdown-toggle rounded" data-bs-toggle="collapse" href="#collapseAdvanceFilter"
-            role="button" aria-expanded="false" aria-controls="collapseAdvanceFilter">
+        <a class="btn btn-inactive dropdown-toggle rounded" data-bs-toggle="collapse" href="#collapseAdvanceFilter{{$type}}"
+            role="button" aria-expanded="false" aria-controls="collapseAdvanceFilter{{$type}}">
             <span class="">Advance Filter</span>
         </a>
     </div>
     @endif
 </div>
-<div class="collapse" id="collapseAdvanceFilter" wire:ignore>
+<div class="collapse" id="collapseAdvanceFilter{{$type}}" wire:ignore>
     <div class="col-lg-12">
         <div class="row">
             
