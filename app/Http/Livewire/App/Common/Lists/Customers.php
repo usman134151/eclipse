2700af823e72c0443ed93e09c9e3716771839299
Maarooshaa Asim <maarooshaa@gmail.com>
2023-09-28 17:44:31 +0500

    users delete enabled

--- app/Http/Livewire/App/Common/Lists/Customers.php
+++ app/Http/Livewire/App/Common/Lists/Customers.php
@@ -128,7 +128,7 @@ final class Customers extends PowerGridComponent
 			return ($model->status);
 		})
 		->addColumn('edit', function(User $model) {
-			return "<div class='d-flex actions'><a href='". route('tenant.customer-edit', ['customerID' => encrypt($model->id)]). "'  title='Edit Customer' aria-label='Edit Team' class='btn btn-sm btn-secondary rounded btn-hs-icon'><svg title='Edit Customer' width='20' height='20' viewBox='0 0 20 20'><use xlink:href='/css/common-icons.svg#pencil'></use></svg></a><a href='" . route('tenant.customer-profile',['customerID'=> encrypt($model->id)]) . "' title='View Customer' aria-label='View Customer' class='btn btn-sm btn-secondary rounded btn-hs-icon'  ><svg aria-label='View Customer' width='20' height='20' viewBox='0 0 20 20'><use xlink:href='/css/common-icons.svg#view'><use></svg></a><div class='d-flex actions'><div class='dropdown ac-cstm'><a href='javascript:void(0)' title='More Options' aria-label='More Options' class='btn btn-sm btn-secondary rounded btn-hs-icon dropdown-toggle' data-bs-toggle='dropdown' data-bs-auto-close='outside' data-bs-popper-config='{&quot;strategy&quot;:&quot;fixed&quot;}'><svg aria-label='More Options' width='20' height='20' viewBox='0 0 20 20'><use xlink:href='/css/common-icons.svg#dropdown'></use></svg></a><div class='tablediv dropdown-menu'><a title='View customer's Invoice' aria-label='View customer's Invoice' href='#' class='dropdown-item'><i class='fa fa-eye'></i>View Customer's Invoices</a><a title='Chat' aria-label='Chat' class='dropdown-item' href='#'><i class='fa fa-comment'></i>Chat</a><a href='javascript:void(0)' aria-label='Deactivate' title='Deactivate' class='dropdown-item'><i class='fa fa-times-circle'></i>Deactivate</a></div></div></div></div>";
+			return "<div class='d-flex actions'><a href='". route('tenant.customer-edit', ['customerID' => encrypt($model->id)]). "'  title='Edit Customer' aria-label='Edit Team' class='btn btn-sm btn-secondary rounded btn-hs-icon'><svg title='Edit Customer' width='20' height='20' viewBox='0 0 20 20'><use xlink:href='/css/common-icons.svg#pencil'></use></svg></a><a href='" . route('tenant.customer-profile',['customerID'=> encrypt($model->id)]) . "' title='View Customer' aria-label='View Customer' class='btn btn-sm btn-secondary rounded btn-hs-icon'  ><svg aria-label='View Customer' width='20' height='20' viewBox='0 0 20 20'><use xlink:href='/css/common-icons.svg#view'><use></svg></a><div class='d-flex actions'><div class='dropdown ac-cstm'><a href='javascript:void(0)' title='More Options' aria-label='More Options' class='btn btn-sm btn-secondary rounded btn-hs-icon dropdown-toggle' data-bs-toggle='dropdown' data-bs-auto-close='outside' data-bs-popper-config='{&quot;strategy&quot;:&quot;fixed&quot;}'><svg aria-label='More Options' width='20' height='20' viewBox='0 0 20 20'><use xlink:href='/css/common-icons.svg#dropdown'></use></svg></a><div class='tablediv dropdown-menu'><a title='View customer's Invoice' aria-label='View customer's Invoice' href='#' class='dropdown-item'><i class='fa fa-eye'></i>View Customer's Invoices</a><a title='Chat' aria-label='Chat' class='dropdown-item' href='#'><i class='fa fa-comment'></i>Chat</a><a href='javascript:void(0)' aria-label='Deactivate' title='Deactivate' class='dropdown-item'><i class='fa fa-times-circle'></i>Deactivate</a><a href='javascript:void(0)' aria-label='Deactivate' title='Delete' wire:click='deleteItem(".$model->id.")' class='dropdown-item'><i class='fa fa-trash'></i>Delete</a></div></div></div></div>";
 		});
 	}
 
@@ -184,6 +184,18 @@ final class Customers extends PowerGridComponent
 		]);
 	}
 
+	public function deleteItem($id){
+		$this->emit('updateRecordId', $id);
+
+		// Dispatches a browser event to show a confirmation modal
+		$this->dispatchBrowserEvent('swal:confirm', [
+			'type' => 'warning',
+			'title' => 'Delete Operation',
+			'text' => 'Are you sure you want to delete this record?',
+		]);
+	}
+
+	
 	// A method to handle the editable columns update event
 	public function onUpdatedEditable(string $id, string $field, string $value): void
 	{
