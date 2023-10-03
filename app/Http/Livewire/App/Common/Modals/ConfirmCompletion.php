e05f6ab8d7915d854bd079c62a1f1e168b431752
Maarooshaa Asim <maarooshaa@gmail.com>
2023-10-03 15:03:59 +0500

    added start and end time to customer-confirm-completion

--- app/Http/Livewire/App/Common/Modals/ConfirmCompletion.php
+++ app/Http/Livewire/App/Common/Modals/ConfirmCompletion.php
@@ -15,7 +15,7 @@ use Livewire\WithFileUploads;
 class ConfirmCompletion extends Component
 {
     use WithFileUploads;
-    public $showForm, $booking_service, $service_details = [], $upload_signature, $status, $notes, $providers = [], $feedback = [];
+    public $showForm, $booking_service, $service_details = [], $upload_signature, $checkout=[], $providers = [], $feedback = [];
     protected $listeners = ['showList' => 'resetForm', 'openConfirmCompletionModal' => 'setBookingId'];
 
     public function setBookingId($booking_service_id)
@@ -29,11 +29,36 @@ class ConfirmCompletion extends Component
             $this->feedback[$provider->provider_id]['feedback_comments'] = null;
         };
         $this->upload_signature = null;
-        $this->status = null;
-        $this->notes = null;
+      
+        $this->checkout = [
+            'customer_status' => '',
+            'customer_notes' => '',
+            'customer_actual_start_date' => Carbon::parse($this->booking_service->start_time)->format('m/d/Y'),
+            'customer_actual_start_hour' => Carbon::parse($this->booking_service->start_time)->format('H'),
+            'customer_actual_start_min' => Carbon::parse($this->booking_service->start_time)->format('i'),
+
+            'customer_actual_end_date' => Carbon::parse($this->booking_service->end_time)->format('m/d/Y'),
+            'customer_actual_end_hour' => Carbon::parse($this->booking_service->end_time)->format('H'),
+            'customer_actual_end_min' => Carbon::parse($this->booking_service->end_time)->format('i'),
+            // 'customer_actual_end_timestamp' => Carbon::today()
+        ];
 
         // dd($this->providers->first());
     }
+    public function mount()
+    {
+        $this->booking_service = new BookingServices();
+        $this->checkout =[
+            'customer_status'=>'',
+            'customer_notes' => '',
+           'customer_actual_end_date' => Carbon::today()->format('m/d/Y'),
+            'customer_actual_end_hour' => Carbon::today()->format('H'),
+            'customer_actual_end_min' => Carbon::today()->format('i'),
+            'customer_actual_end_timestamp' => Carbon::today()->format('i')
+
+        ];
+    }
+
     public function render()
     {
         return view('livewire.app.common.modals.confirm-completion');
@@ -47,31 +72,40 @@ class ConfirmCompletion extends Component
     {
         return  [
             'upload_signature' => 'nullable|file|mimes:png,jpg,jpeg,gif,bmp,svg,pdf,doc,docx,xls,xlsx,ppt,pptx,txt,rtf,zip,rar,tar.gz,tgz,tar.bz2,tbz2,7z,mp3,wav,aac,flac,wma,mp4,avi,mov,wmv,mkv,csv',
+            'checkout.customer_actual_end_date' => 'required|date|after:checked_in_details.actual_start_date',
+            'checkout.customer_actual_end_hour' => 'required|numeric|between:0,23',
+            'checkout.customer_actual_end_min' => 'required|numeric|between:0,59',
+            'checkout.customer_actual_start_date' => 'required|date',
+            'checkout.customer_actual_start_hour' => 'required|numeric|between:0,23',
+            'checkout.customer_actual_start_min' => 'required|numeric|between:0,59'
         ];
     }
 
 
     public function save()
     {
-       
+
         $this->validate();
+        // dd($this->checkout);
         foreach ($this->providers as $provider) {
-            $checkout =$provider->check_out_procedure_values ?? [];
+            $p_checkout = $provider->check_out_procedure_values ?? [];
             if ($this->upload_signature) {
                 $fileService = new UploadFileService();
-                $checkout['customer_signature'] = $fileService->saveFile('bookings/' . $this->booking_service->booking_id, $this->upload_signature, null, $this->booking_service->id . '_' . time() . '_checkout_customer_signature');
+                $p_checkout['customer_signature'] = $fileService->saveFile('bookings/' . $this->booking_service->booking_id, $this->upload_signature, null, $this->booking_service->id . '_' . time() . '_checkout_customer_signature');
             }
-            $checkout['customer_rating'] = $this->feedback[$provider->provider_id]['rating'];
-            $checkout['customer_feedback_comments'] = $this->feedback[$provider->provider_id]['feedback_comments'];
-            $checkout['customer_actual_end_date'] = Carbon::today()->format('m/d/Y');
-            $checkout['customer_actual_end_hour'] = Carbon::today()->format('H');
-            $checkout['customer_actual_end_min'] = Carbon::today()->format('i');
-            $checkout['customer_actual_end_timestamp'] = Carbon::today()->format('i');
-            $checkout['customer_entry_notes'] = $this->notes;
-            $checkout['customer_checkout_status'] = $this->status;
-            $checkout['customer_close_out_by'] = Auth::id();
+            $p_checkout['customer_rating'] = $this->feedback[$provider->provider_id]['rating'];
+            $p_checkout['customer_feedback_comments'] = $this->feedback[$provider->provider_id]['feedback_comments'];
+            $p_checkout['customer_actual_end_date'] = $this->checkout['customer_actual_end_date'];
+            $p_checkout['customer_actual_end_hour'] = $this->checkout['customer_actual_end_hour'];
+            $p_checkout['customer_actual_end_min'] = $this->checkout['customer_actual_end_min'];
+            $p_checkout['customer_actual_start_date'] = $this->checkout['customer_actual_start_date'];
+            $p_checkout['customer_actual_start_hour'] = $this->checkout['customer_actual_start_hour'];
+            $p_checkout['customer_actual_start_min'] = $this->checkout['customer_actual_start_min'];
+            $p_checkout['customer_entry_notes'] = $this->checkout['customer_notes'];
+            $p_checkout['customer_checkout_status'] = $this->checkout['customer_status'];
+            $p_checkout['customer_close_out_by'] = Auth::id();
 
-            $provider->check_out_procedure_values = $checkout;
+            $provider->check_out_procedure_values = $p_checkout;
             $provider->check_in_status = 3;
             $provider->return_status = 1;
 
@@ -88,12 +122,12 @@ class ConfirmCompletion extends Component
                 'comments' => $this->feedback[$provider->provider_id]['feedback_comments'],
             ]);
         }
-       
-        
-            Booking::where('id',$this->booking_service->booking_id)->update(['is_closed' => true]);
+
+
+        Booking::where('id', $this->booking_service->booking_id)->update(['is_closed' => true]);
 
         $this->emit('closeConfirmCompletionModal');
-        $this->emit('showConfirmation','Booking service has been successfully closed!');
+        $this->emit('showConfirmation', 'Booking service has been successfully closed!');
     }
 
     function showForm()
--- resources/views/livewire/app/common/modals/confirm-completion.blade.php
+++ resources/views/livewire/app/common/modals/confirm-completion.blade.php
@@ -6,6 +6,194 @@
     <div class="modal-body">
         <div class="row">
 
+            <div class="col-lg-12 inner-section-segment-spacing">
+                <div class="border-bottom border-dark border-start-0 border-end-0 border-top-0 py-3">
+                    <div class="row">
+                        <div class="col-lg-2 align-self-center">
+                            <label class="form-label-sm fw-semibold mb-lg-0">Start Date:</label>
+                        </div>
+                        <div class="col-lg-3 align-self-center">
+                            <div class="text-sm">
+                                {{ date_format(date_create($booking_service->start_time), 'd F Y') }}
+
+                            </div>
+                        </div>
+                        <div class="col-lg-3 align-self-center">
+                            <label class="form-label-sm fw-semibold mb-lg-0">Actual Start Date:</label>
+                        </div>
+                        <div class="col-lg-4 align-self-center">
+                            {{-- @if (isset($this->checkout['actual_start_timestamp']))
+                                    <div class="text-sm">
+                                        {{ $checkout['actual_start_timestamp'] ? date_format(date_create($checkout['actual_start_timestamp']), 'd F Y') : '' }}</label>
+                                    </div>
+                                @else --}}
+                            <div class="position-relative">
+                                <svg aria-label="Date" class="icon-date md cursor-pointer" width="20" height="20"
+                                    viewBox="0 0 20 20">
+                                    <use xlink:href="/css/common-icons.svg#datefield-icon">
+                                    </use>
+                                </svg>
+                                <input class="form-control form-control-md js-single-date"
+                                    wire:model="checkout.customer_actual_start_date" id="actual_start_date"
+                                    placeholder="MM/DD/YYYY" name="actual_start_date" aria-label="Select Date">
+                                     
+                            </div>
+                            
+                            {{-- @endif --}}
+                        </div>
+
+                    </div>
+                    
+                </div>
+                 @error('checkout.customer_actual_start_date')
+                                    <span class="d-inline-block invalid-feedback mt-2">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                <div class="border-bottom border-dark border-start-0 border-end-0 border-top-0 py-3">
+                    <div class="row">
+                        <div class="col-lg-2 align-self-center">
+                            <label class="form-label-sm fw-semibold mb-lg-0">End Date:</label>
+                        </div>
+                        <div class="col-lg-3 align-self-center">
+                            <div class="text-sm">
+                                {{ date_format(date_create($booking_service->end_time), 'd F Y') }}
+
+                            </div>
+                        </div>
+                        <div class="col-lg-3 align-self-center">
+                            <label class="form-label-sm fw-semibold mb-lg-0">Actual End Date:</label>
+                        </div>
+                        <div class="col-lg-4 align-self-center">
+                            <div class="position-relative">
+                                <svg aria-label="Date" class="icon-date md cursor-pointer" width="20" height="20"
+                                    viewBox="0 0 20 20">
+                                    <use xlink:href="/css/common-icons.svg#datefield-icon">
+                                    </use>
+                                </svg>
+                                <input class="form-control form-control-md js-single-date"
+                                    wire:model="checkout.customer_actual_end_date" placeholder="MM/DD/YYYY"
+                                    name="actual_end_date" aria-label="Select Date" id="actual_end_date">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @error('checkout.customer_actual_end_date')
+                                    <span class="d-inline-block invalid-feedback mt-2">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                <div class="border-bottom border-dark border-start-0 border-end-0 border-top-0 py-3">
+                    <div class="row">
+                        <div class="col-lg-2 align-self-center">
+                            <label class="form-label-sm fw-semibold mb-lg-0">Start Time:</label>
+                        </div>
+                        <div class="col-lg-3 align-self-center">
+                            <div class="text-sm">
+                                {{ date_format(date_create($booking_service->start_time), 'h:i A') }}
+                            </div>
+                        </div>
+                        <div class="col-lg-3 align-self-center">
+                            <label class="form-label-sm fw-semibold mb-lg-0">Actual Start Time:</label>
+                        </div>
+                        <div class="col-lg-4 align-self-center">
+                            {{-- @if (isset($this->checkout['actual_start_timestamp']))
+                                    <div class="text-sm">
+                                        {{ $checkout['actual_start_timestamp'] ? date_format(date_create($checkout['actual_start_timestamp']), 'h:i A') : '' }}</label>
+                                    </div>
+                                @else --}}
+                            <div class="d-flex gap-2">
+                                <div class="time d-flex align-items-center gap-2">
+                                    <div>
+                                        <input class="form-control form-control-sm text-center hours"
+                                            id="actual_start_hour" aria-label="Start Time" name="actual_start_hour"
+                                            placeholder="00" type="number" tabindex=""
+                                            wire:model.defer="checkout.customer_actual_start_hour" maxlength="2">
+                                    </div>
+                                    <svg width="5" height="19" viewBox="0 0 5 19" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M0.652588 16.6132C0.652588 16.1098 0.807878 15.6868 1.11846 15.3441C1.43975 14.9907 1.90026 14.814 2.5 14.814C3.09974 14.814 3.5549 14.9907 3.86548 15.3441C4.18677 15.6868 4.34741 16.1098 4.34741 16.6132C4.34741 17.1058 4.18677 17.5235 3.86548 17.8662C3.5549 18.2089 3.09974 18.3803 2.5 18.3803C1.90026 18.3803 1.43975 18.2089 1.11846 17.8662C0.807878 17.5235 0.652588 17.1058 0.652588 16.6132ZM0.668652 2.42827C0.668652 1.92492 0.823942 1.50189 1.13452 1.15918C1.45581 0.805761 1.91632 0.629052 2.51606 0.629052C3.1158 0.629052 3.57096 0.805761 3.88154 1.15918C4.20283 1.50189 4.36348 1.92492 4.36348 2.42827C4.36348 2.92091 4.20283 3.33859 3.88154 3.6813C3.57096 4.02401 3.1158 4.19536 2.51606 4.19536C1.91632 4.19536 1.45581 4.02401 1.13452 3.6813C0.823942 3.33859 0.668652 2.92091 0.668652 2.42827Z"
+                                            fill="black" />
+                                    </svg>
+                                    <div>
+                                        <input class="form-control form-control-sm text-center  mins"
+                                            aria-label="Start Minutes" id="actual_start_min" name="actual_start_min"
+                                            placeholder="00" type="number" tabindex=""
+                                            wire:model.defer="checkout.customer_actual_start_min" maxlength="2">
+                                    </div>
+                                </div>
+                            </div>
+                            {{-- @endif --}}
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex">
+                    @error('checkout.customer_actual_start_hour')
+                        <span class="d-inline-block invalid-feedback mt-2">
+                            {{ $message }}
+                        </span>
+                    @enderror
+                    @error('checkout.customer_actual_start_min')
+                        <span class="d-inline-block invalid-feedback mt-2">
+                            {{ $message }}
+                        </span>
+                    @enderror
+                </div>
+                <div class="border-bottom border-dark border-start-0 border-end-0 border-top-0 py-3">
+                    <div class="row">
+                        <div class="col-lg-2 align-self-center">
+                            <label class="form-label-sm fw-semibold mb-lg-0">End Time:</label>
+                        </div>
+                        <div class="col-lg-3 align-self-center">
+                            <div class="text-sm">
+                                {{ date_format(date_create($booking_service->end_time), 'h:i A') }}
+
+                            </div>
+                        </div>
+                        <div class="col-lg-3 align-self-center">
+                            <label class="form-label-sm fw-semibold mb-lg-0">Actual End Time:</label>
+                        </div>
+                        <div class="col-lg-4 align-self-center">
+                            <div class="d-flex gap-2">
+                                <div class="time d-flex align-items-center gap-2">
+                                    <div>
+                                        <input class="form-control form-control-sm text-center hours"
+                                            id="actual_end_hour" aria-label="Start Time" name="actual_end_hour"
+                                            placeholder="00" type="number" tabindex=""
+                                            wire:model.defer="checkout.customer_actual_end_hour" maxlength="2">
+                                    </div>
+                                    <svg width="5" height="19" viewBox="0 0 5 19" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M0.652588 16.6132C0.652588 16.1098 0.807878 15.6868 1.11846 15.3441C1.43975 14.9907 1.90026 14.814 2.5 14.814C3.09974 14.814 3.5549 14.9907 3.86548 15.3441C4.18677 15.6868 4.34741 16.1098 4.34741 16.6132C4.34741 17.1058 4.18677 17.5235 3.86548 17.8662C3.5549 18.2089 3.09974 18.3803 2.5 18.3803C1.90026 18.3803 1.43975 18.2089 1.11846 17.8662C0.807878 17.5235 0.652588 17.1058 0.652588 16.6132ZM0.668652 2.42827C0.668652 1.92492 0.823942 1.50189 1.13452 1.15918C1.45581 0.805761 1.91632 0.629052 2.51606 0.629052C3.1158 0.629052 3.57096 0.805761 3.88154 1.15918C4.20283 1.50189 4.36348 1.92492 4.36348 2.42827C4.36348 2.92091 4.20283 3.33859 3.88154 3.6813C3.57096 4.02401 3.1158 4.19536 2.51606 4.19536C1.91632 4.19536 1.45581 4.02401 1.13452 3.6813C0.823942 3.33859 0.668652 2.92091 0.668652 2.42827Z"
+                                            fill="black" />
+                                    </svg>
+                                    <div>
+                                        <input class="form-control form-control-sm text-center  mins"
+                                            aria-label="Start Minutes" id="actual_end_min" name="actual_end_min"
+                                            placeholder="00" type="number" tabindex=""
+                                            wire:model.defer="checkout.customer_actual_end_min" maxlength="2">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex">
+                    @error('checkout.customer_actual_end_hour')
+                        <span class="d-inline-block invalid-feedback mt-2">
+                            {{ $message }}
+                        </span>
+                    @enderror
+                    @error('checkout.customer_actual_end_min')
+                        <span class="d-inline-block invalid-feedback mt-2">
+                            {{ $message }}
+                        </span>
+                    @enderror
+                </div>
+            </div>
             <div class="col-lg-0  gap-2">
                 <div class="d-flex justify-between">
                     <div class="col-8 ">
@@ -55,8 +243,8 @@
                                 isset($service_details['status_types']['completed']) &&
                                 $service_details['status_types']['completed'] == true)
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="booking-status" id="booking-status"
-                                    wire:model.lazy="status" value="completed">
+                                <input class="form-check-input" type="radio" name="booking-status"
+                                    id="booking-status" wire:model.lazy="checkout.customer_status" value="completed">
                                 <label class="form-check-label" for="completed">
                                     Completed
                                 </label>
@@ -66,8 +254,8 @@
                                 isset($service_details['status_types']['noshow']) &&
                                 $service_details['status_types']['noshow'] == true)
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="booking-status" id="booking-status"
-                                    wire:model.lazy="status" value="noshow">
+                                <input class="form-check-input" type="radio" name="booking-status"
+                                    id="booking-status" wire:model.lazy="checkout.customer_status" value="noshow">
                                 <label class="form-check-label" for="no-show">
                                     No Show
                                 </label>
@@ -77,8 +265,8 @@
                                 isset($service_details['status_types']['cancelled']) &&
                                 $service_details['status_types']['cancelled'] == true)
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="booking-status" id="booking-status"
-                                    wire:model.lazy="status" value="cancelled">
+                                <input class="form-check-input" type="radio" name="booking-status"
+                                    id="booking-status" wire:model.lazy="checkout.customer_status" value="cancelled">
                                 <label class="form-check-label" for="cancellation">
                                     Cancellation
                                 </label>
@@ -94,7 +282,7 @@
                             <use xlink:href="/css/provider.svg#fill-question"></use>
                         </svg>
                     </label>
-                    <textarea wire:model.lazy="notes" class="form-control" rows="3" placeholder="" name="notesColumn"
+                    <textarea wire:model.lazy="checkout.customer_notes" class="form-control" rows="3" placeholder="" name="notesColumn"
                         id="notes-column"></textarea>
                 </div>
             </div>
@@ -116,7 +304,7 @@
                     @foreach ($providers as $i => $provider)
                         <tr role="row" class="odd">
 
-                            <td wire:ignore>
+                            <td wire:ignore class="w-50">
                                 <div class="row g-2">
                                     <div class="col-md-2">
                                         <img src="{{ $provider->profile_pic ? $provider->profile_pic : '/tenant-resources/images/portrait/small/avatar-s-20.jpg' }}"
@@ -158,7 +346,7 @@
                                     </div>
                                 </div>
                             </td>
-                            <td>
+                            <td class="w-100">
                                 <textarea class="form-control" name="" id="" cols="30" rows="1"
                                     wire:model.lazy="feedback.{{ $provider->provider_id }}.feedback_comments" placeholder="Enter Feedback"></textarea>
                             </td>
