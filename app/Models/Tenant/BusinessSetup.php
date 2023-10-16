2d0fc6df6e92128fb243cce59f452713476b5264
HammadJ <hammadjavaid2@gmail.com>
2023-10-16 11:01:13 +0500

    added business name field and remove required from sub domain

--- app/Models/Tenant/BusinessSetup.php
+++ app/Models/Tenant/BusinessSetup.php
@@ -12,10 +12,10 @@ class BusinessSetup extends Model
     /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var array 
      */
     protected $fillable = [
-        'user_id' , 'business_start_time' , 'business_end_time' , 'after_start_time' , 'after_end_time' ,
+        'user_id' , 'business_name', 'business_start_time' , 'business_end_time' , 'after_start_time' , 'after_end_time' ,
         'default_colour','foreground_color','portal_url','company_logo','login_screen','welcome_text','notification_email','response_email',
         'staff_providers', 'contract_providers', 'feedback', 'deposit_form_file', 'require_provider_approval', 'rate_for_providers', 'measurement_providers',
         'rate_for_travel_time', 'currency', 'billing_days', 'service_agreements_file', 'service_url_link', 'send_qoutes',
