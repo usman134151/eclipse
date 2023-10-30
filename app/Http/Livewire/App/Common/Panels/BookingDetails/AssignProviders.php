dd220f8c6e2ec971cb50faa5f5ada86124c68732
Amna Bilal <amnausman@gmail.com>
2023-10-30 13:13:33 +0500

    null checks

--- app/Http/Livewire/App/Common/Panels/BookingDetails/AssignProviders.php
+++ app/Http/Livewire/App/Common/Panels/BookingDetails/AssignProviders.php
@@ -351,7 +351,7 @@ class AssignProviders extends Component
 
                         
                         $this->providersPayment[$index] = $aProvider; //overriding if already assigned
-                        if(!key_exists('specialization_charges',$aProvider['service_payment_details'])){
+                        if(!is_null($aProvider['service_payment_details']) && is_array($aProvider['service_payment_details']) && !key_exists('specialization_charges',$aProvider['service_payment_details'])){
                             $this->providersPayment[$index]['service_payment_details']['specialization_charges']=[];
                         }
                         if (isset($aProvider['total_amount']) && $aProvider['total_amount'] == "0.00") {
