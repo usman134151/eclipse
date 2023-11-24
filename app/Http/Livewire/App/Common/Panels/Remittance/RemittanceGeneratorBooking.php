0d21f11a0a34acc32c010fb68728760c4ebade37
Maarooshaa Asim <maarooshaa@gmail.com>
2023-11-24 13:16:46 +0000

    remittances-activation

--- app/Http/Livewire/App/Common/Panels/Remittance/RemittanceGeneratorBooking.php
+++ app/Http/Livewire/App/Common/Panels/Remittance/RemittanceGeneratorBooking.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Livewire\App\Common\Panels\Remittance;
 
+use App\Models\Tenant\BookingProvider;
+use App\Models\Tenant\BookingReimbursement;
+use App\Models\Tenant\User;
 use Livewire\Component;
 
 class RemittanceGeneratorBooking extends Component
 {
-    public $showForm;
+    public $showForm, $provider,$data=[];
     protected $listeners = ['showList' => 'resetForm'];
 
     public function render()
@@ -14,10 +17,21 @@ class RemittanceGeneratorBooking extends Component
         return view('livewire.app.common.panels.remittance.remittance-generator-booking');
     }
 
-    public function mount()
+    public function mount($providerId)
     {
-       
-       
+       $this->provider = User::where('id',$providerId)->with('userdetail')->first()->toArray();
+       $bookings = BookingProvider::where(['provider_id'=>$providerId,'payment_status'=>0])
+       ->with('booking')
+       ->select('booking_id')
+       ->selectRaw('CASE WHEN is_override_price = 1
+               THEN override_price
+               ELSE total_amount
+          END AS amount')
+       ->get()->toArray();
+       $reimbursements = BookingReimbursement::where(['provider_id'=>$providerId,'payment_status'=>0])->select(['id as reimbursement_id','amount','booking_id'])->get()->toArray();
+       $this->data = array_merge($bookings,$reimbursements);
+
+    
     }
 
     function showForm()
