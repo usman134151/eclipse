cb1b73abada8496011e8d7dd40b898e292302b6a
Maarooshaa Asim <maarooshaa@gmail.com>
2023-11-27 18:24:35 +0500

    fetch bookings for remittance

--- app/Http/Livewire/App/Common/Panels/Remittance/IssueRemittance.php
+++ app/Http/Livewire/App/Common/Panels/Remittance/IssueRemittance.php
@@ -24,7 +24,7 @@ class IssueRemittance extends Component
                 // fetch reimbursement data
             } else {
                 //fetch booking details + associated reimbursements
-                // $this->list = BookingProvider::where(['provider_id'=>$provider, 'booking_id'])
+                $this->list[] = BookingProvider::where(['provider_id'=>$providerId, 'booking_id'=>$row['booking_id']])->first();
             }
         }
     }
--- resources/views/livewire/app/common/panels/remittance/issue-remittance.blade.php
+++ resources/views/livewire/app/common/panels/remittance/issue-remittance.blade.php
@@ -103,6 +103,97 @@
                     </thead>
                     <tbody>
                         {{-- updated by shanila to reduce extra duplicate rows --}}
+                        @foreach($list as $row)
+                        <tr role="row" class="even">
+                            <td class="text-center">
+                                <input class="form-check-input" type="checkbox" value="" aria-label="Select Team">
+                            </td>
+                            <td>
+                                <div class="fw-semibold">{{$row->booking->booking_number}}</div>
+                                {{-- display separate time for each service --}}
+                                <div>
+                                    <div>{{formatDate($row->booking->booking_start_at)}}</div>
+                                    <div class="d-inline-flex" data-bs-toggle="tooltip" data-bs-html="true"
+                                        data-bs-title="<div><b>Payment Notes</b></div> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna Lorem ipsum dolor sit amet</p>">
+                                        {{formatTime($row->booking->booking_start_at)}} to {{formatTime($row->booking->booking_end_at)}}</div>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="text-sm">
+                                    {{$row->booking->company ? $row->booking->company->name : 'N/A'}}
+                                </div>
+                                <div class="text-sm">
+                                    Requester: {{$row->booking->customer ? $row->booking->customer->name : 'N/A'}}
+                                </div>
+                                <div class="text-sm">
+                                    Supervisor: {{$row->booking->booking_supervisor ? $row->booking->booking_supervisor->name : 'N/A'}}
+                                </div>
+                                <div class="text-sm">
+                                    Billing Manager: {{$row->booking->billing_manager ? $row->booking->billing_manager->name : 'N/A'}}
+                                </div>
+                                <div class="text-sm">
+                                    Service Consumer(s): 
+                                </div>
+                            </td>
+                            <td class="position-relative">
+                                <a href="#" title="Edit" aria-label="Edit"
+                                    class="btn btn-sm btn-secondary rounded btn-hs-icon icon-edit">
+                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M19.2555 4.11766L15.8304 0.680256C15.3834 0.258855 14.7977 0.017061 14.1846 0.000869084C13.5715 -0.0153228 12.9739 0.195217 12.5054 0.592439L1.25527 11.8832C0.85122 12.2921 0.599641 12.8281 0.54276 13.4012L0.00525375 18.6325C-0.0115852 18.8163 0.0121717 19.0015 0.074831 19.175C0.13749 19.3485 0.237509 19.5059 0.367758 19.6362C0.484559 19.7524 0.623081 19.8444 0.775379 19.9069C0.927678 19.9693 1.09076 20.0009 1.25527 20H1.36777L6.58033 19.5233C7.15133 19.4662 7.68538 19.2137 8.09284 18.8082L19.343 7.51743C19.7796 7.05447 20.0156 6.43667 19.9992 5.7994C19.9828 5.16213 19.7154 4.55738 19.2555 4.11766ZM6.35532 17.0142L2.60528 17.3655L2.94279 13.6019L10.0054 6.60163L13.3804 9.98885L6.35532 17.0142ZM15.0054 8.30778L11.6554 4.94565L14.0929 2.43659L17.5054 5.86145L15.0054 8.30778Z"
+                                            fill="black"></path>
+                                    </svg>
+                                </a>
+                                <div class="">Billable Hours: 10</div>
+                                <div class="">Hourly Rate: $10</div>
+                                <div>Total Service Rate: $100</div>
+                                <div class="text-primary"><small>Additional Charges:</small></div>
+                                <div>Fuel Charges: $20 <i class="fa fa-check-circle" data-bs-toggle="tooltip"
+                                        data-bs-placement="top" title=""></i></div>
+                                <div>Fuel Charges: $15</div>
+                                <div class="text-primary"><small class="text-xxs">Charged To Customer</small></div>
+                            </td>
+                            <td class="">
+                                <div>$135</div>
+                                <div>$15 (Pending)</div>
+                            </td>
+                            <td>
+                                <div class="d-flex align-items-center gap-1">
+                                    <svg class="fill-danger" xmlns="http://www.w3.org/2000/svg" width="12"
+                                        height="12" viewBox="0 0 512 512">
+                                        <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                        <path
+                                            d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512z">
+                                        </path>
+                                    </svg>
+                                    Pending
+                                </div>
+                            </td>
+                            <td>
+                                <div class="d-flex actions justify-content-center">
+                                    <a href="#" title="View" aria-label="View"
+                                        class="btn btn-sm btn-secondary rounded btn-hs-icon">
+                                        <svg class="fill-stroke" width="17" height="18" viewBox="0 0 17 18"
+                                            fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M8.3125 16.8125H1.4375V1.1875H14.5625V9.3125M15.8125 12.4375L11.4375 16.8125M5.1875 8.6875H10.8125M5.1875 12.4375H7.0625M5.1875 4.9375H10.8125M11.4375 12.4375L15.8125 16.8125"
+                                                stroke="black" stroke-width="1.5" stroke-linecap="round"
+                                                stroke-linejoin="round" />
+                                        </svg>
+                                    </a>
+                                    <a href="#" title="Accept" aria-label="Accept"
+                                        class="btn btn-sm btn-secondary rounded btn-hs-icon">
+                                        <svg width="19" height="20" viewBox="0 0 19 20" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M0 0V18.5714H11.6923V17.1429H1.46154V1.42857H8.76923V5.71429H13.1538V7.14286H14.6154V4.71429L14.3962 4.5L10.0115 0.214286L9.79231 0H0ZM10.2308 2.42857L12.1308 4.28571H10.2308V2.42857ZM2.92308 7.14286V8.57143H11.6923V7.14286H2.92308ZM15.3462 8.57143V10C14.1038 10.2143 13.1538 11.2143 13.1538 12.5C13.1538 13.9286 14.25 15 15.7115 15H16.4423C17.0269 15 17.5385 15.5 17.5385 16.0714C17.5385 16.6429 17.0269 17.1429 16.4423 17.1429H13.8846V18.5714H15.3462V20H16.8077V18.5714C18.05 18.3571 19 17.3571 19 16.0714C19 14.6429 17.9038 13.5714 16.4423 13.5714H15.7115C15.1269 13.5714 14.6154 13.0714 14.6154 12.5C14.6154 11.9286 15.1269 11.4286 15.7115 11.4286H18.2692V10H16.8077V8.57143H15.3462ZM2.92308 10.7143V12.1429H8.03846V10.7143H2.92308ZM9.5 10.7143V12.1429H11.6923V10.7143H9.5ZM2.92308 13.5714V15H8.03846V13.5714H2.92308ZM9.5 13.5714V15H11.6923V13.5714H9.5Z" />
+                                        </svg>
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
+                        @endforeach
                         <tr role="row" class="even">
                             <td class="text-center">
                                 <input class="form-check-input" type="checkbox" value="" aria-label="Select Team">
