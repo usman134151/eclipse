e91c7a2b2b487d8c8b99e52e862b13af3a115b44
Maarooshaa Asim <maarooshaa@gmail.com>
2023-11-27 17:09:26 +0500

    issue - remittance

--- app/Http/Livewire/App/Common/Panels/Remittance/IssueRemittance.php
+++ app/Http/Livewire/App/Common/Panels/Remittance/IssueRemittance.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Livewire\App\Common\Panels\Remittance;
 
+use App\Models\Tenant\BookingProvider;
 use App\Models\Tenant\User;
 use Livewire\Component;
 
 class IssueRemittance extends Component
 {
-    public $showForm, $selectedRows=[], $provider;
+    public $showForm, $list = [], $provider;
     protected $listeners = ['showList' => 'resetForm', 'issueRemittances'];
 
     public function render()
@@ -15,19 +16,25 @@ class IssueRemittance extends Component
         return view('livewire.app.common.panels.remittance.issue-remittance');
     }
 
-    public function mount($providerId)
+    public function mount($selectedRows,$providerId)
     {
         $this->provider = User::find($providerId);
-       
+        foreach ($this->selectedRows as $row) {
+            if (key_exists('reimbursement_id', $row)) {
+                // fetch reimbursement data
+            } else {
+                //fetch booking details + associated reimbursements
+                // $this->list = BookingProvider::where(['provider_id'=>$provider, 'booking_id'])
+            }
+        }
     }
 
     function showForm()
-    {     
-       $this->showForm=true;
+    {
+        $this->showForm = true;
     }
     public function resetForm()
     {
-        $this->showForm=false;
+        $this->showForm = false;
     }
-
 }
