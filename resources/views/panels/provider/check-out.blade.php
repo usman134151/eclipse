053bc52b1e1e3fdf5f8c0846e3f7b8a4d6d1533a
Maarooshaa Asim <maarooshaa@gmail.com>
2023-12-07 08:07:22 +0000

    checkout fix from assignment details

--- resources/views/panels/provider/check-out.blade.php
+++ resources/views/panels/provider/check-out.blade.php
@@ -1,7 +1,8 @@
-<x-off-canvas show="offcanvasOpenCheckOut" style="z-index:10569">
+<x-off-canvas show="offcanvasOpenCheckOut" style="z-index:105659">
 
     <x-slot name="title">Check-Out # {{ $bookingNumber }}</x-slot>
-    @if ($checkout_booking_id && $providerPanelType == 2)
+
+    @if ($checkout_booking_id)
         @livewire('app.common.panels.provider.check-out', ['booking_id' => $checkout_booking_id, 'booking_service_id' => $booking_service_id, 'provider_id' => $selectedProvider])
     @endif
 </x-off-canvas>
@@ -10,7 +11,14 @@
         var booking_id = event.detail.booking_id;
         var booking_service_id = event.detail.booking_service_id;
 
+        console.log('emitted back');
 
         Livewire.emit('showCheckOutPanel', booking_id, booking_service_id);
+    });
+
+    window.addEventListener('open-provider-check-out', function(event) {
+        var booking_id = event.detail.booking_id;
+        var booking_service_id = event.detail.booking_service_id;
+        Livewire.emit('openProviderCheckOut', booking_id, booking_service_id);
     });
 </script>
