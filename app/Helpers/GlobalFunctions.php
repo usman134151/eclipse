ef876381833a1cb1161fa94861f7e68f845779f8
Amna Bilal <amnausman@gmail.com>
2023-12-07 19:30:34 +0500

    fix issue after 24 hour format

--- app/Helpers/GlobalFunctions.php
+++ app/Helpers/GlobalFunctions.php
@@ -78,9 +78,12 @@ const DECLINED = '2';
     // Try to determine if the time is in a 12-hour or 24-hour format
     if (preg_match('/\b(?:[01]?[0-9]|2[0-3]):[0-5][0-9]\b/', $date)) {
       // 24-hour format
-      $date=split(' ',$date);
+      $date=explode(' ',$date);
+      if(count($date)>0)
       return $date[1];
-      return Carbon::createFromFormat('H:i', $date)->format('g:i');
+      else
+       return $date;
+      //return Carbon::createFromFormat('H:i', $date)->format('g:i');
     } elseif (preg_match('/\b(?:[01]?[0-9]|2[0-3]):[0-5][0-9] (AM|PM)\b/i', $date)) {
       // 12-hour format with AM/PM
       return Carbon::createFromFormat('g:i A', $date)->format('g:i A');
