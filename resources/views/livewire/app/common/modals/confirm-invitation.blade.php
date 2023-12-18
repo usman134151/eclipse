4e0c6d64a9de3d71a43be0edfac284064ac75f02
Maarooshaa Asim <maarooshaa@gmail.com>
2023-12-18 09:29:48 +0000

    Invitation response fixes

--- resources/views/livewire/app/common/modals/confirm-invitation.blade.php
+++ resources/views/livewire/app/common/modals/confirm-invitation.blade.php
@@ -1,6 +1,6 @@
 <div class="modal-content">
     <div class="modal-header">
-        <h2 class="modal-title fs-5 text-center" id="payInvoiceLabel">Confirm Invitation</h2>
+        <h2 class="modal-title fs-5 text-center" id="confirmInvitationLabel">Confirm Invitation</h2>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
     </div>
     <div class="modal-body">
