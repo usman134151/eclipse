afe1f382ea81545a9c3894df2febaa373e470d0e
HammadJ <hammadjavaid2@gmail.com>
2023-12-19 17:44:11 +0500

    LBT-171 ui changes in pagination goto page

--- resources/views/livewire/app/common/bookings/booking-nav.blade.php
+++ resources/views/livewire/app/common/bookings/booking-nav.blade.php
@@ -1,69 +1,86 @@
 {{-- <div class="col-xl-6 col-md-6 col-sm-12">
-                    @if ($paginator->lastPage() > 1)
-                        <nav aria-label="Page navigation">
-                            <ul class="pagination">
-                                <li class="page-item {{ ($paginator->currentPage() == 1) ? ' disabled' : '' }}">
-                                    <a class="page-link" href="{{ $paginator->previousPageUrl() }}" wire:click="previousPage">
-                                        <span aria-hidden="true"><i class="fas fa-chevron-left" aria-hidden="true"></i>
-                                        </span>
-                                        <span class="sr-only">Previous</span>
-                                    </a>
-                                </li>
-                                @for ($i = 1; $i <= $paginator->lastPage(); $i++)
-                                    <li class="page-item {{ ($paginator->currentPage() == $i) ? ' active' : '' }}">
-                                        <a class="page-link" href="{{ $paginator->url($i) }}">{{ $i }}</a>
-                                    </li>
-                                @endfor
-                                <li class="page-item {{ ($paginator->currentPage() == $paginator->lastPage()) ? ' disabled' : '' }}">
-                                    <button  class="page-link" wire:click="nextPage">Next</button>
-                                </li>
-                            </ul>
-                        </nav>
-                    @endif
-                </div> --}}
-                <div class="d-flex flex-column flex-md-row justify-content-end">
-                <small class="text-muted d-block mb-2 my-md-2 me-1">
-                        Showing
-                        <span class="font-semibold">{{$paginator->currentPage()}}</span>
-                        of
-                        <span class="font-semibold">{{$paginator->lastPage()}}</span>
-                                                Pages
-                    </small>
-                            
-                    @if ($paginator->hasPages())
-                                
-                                <nav aria-label="Page Navigation">
-                                    <ul class="pagination justify-content-start justify-content-lg-end ">
-                                        <li class="page-item {{ ($paginator->currentPage() == 1) ? ' disabled' : '' }}" >
-                                            <a class="page-link"   wire:click="previousPage" aria-label="Previous">
-                                                <span aria-hidden="true">&laquo;</span>
-                                            </a>
-                                        </li>
-                                        @for ($i = 1; $i <= $paginator->lastPage(); $i++)
-                                            <li class="page-item {{ ($paginator->currentPage() == $i) ? ' active' : '' }}">
-                                                <a class="page-link" href="#" wire:click="gotoPage({{$i}})">{{ $i }}</a>
-                                            </li>
-                                        @endfor
-                                      
-                                        <li class="page-item {{ ($paginator->currentPage() == $paginator->lastPage()) ? ' disabled' : '' }}">
-                                            <a class="page-link"  wire:click="nextPage" aria-label="Next">
-                                                <span aria-hidden="true">&raquo;</span>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                    <ul class="input-group input-group-sm mb-2 gap-2">
-                                        <input type="text" id="pageNumberInput" class="form-control form-control-sm text-center" placeholder="Enter page number">
-                                        <button onclick="gotoLivewirePage()" class="btn btn-primary btn-sm">Go to Page</button>
-                                    </ul>
-                                    <ul>
-                                        <small id="errorMessage" class="text-danger" style="display: none;"></small>
-                                    </ul>
-                                </nav>
-                    @endif
-                    </div>
-@push('scripts')                        
-    <script>
-        function gotoLivewirePage() {
+    @if ($paginator->lastPage() > 1)
+    <nav aria-label="Page navigation">
+        <ul class="pagination">
+            <li class="page-item {{ ($paginator->currentPage() == 1) ? ' disabled' : '' }}">
+                <a class="page-link" href="{{ $paginator->previousPageUrl() }}" wire:click="previousPage">
+                    <span aria-hidden="true"><i class="fas fa-chevron-left" aria-hidden="true"></i>
+                    </span>
+                    <span class="sr-only">Previous</span>
+                </a>
+            </li>
+            @for ($i = 1; $i <= $paginator->lastPage(); $i++)
+                <li class="page-item {{ ($paginator->currentPage() == $i) ? ' active' : '' }}">
+                    <a class="page-link" href="{{ $paginator->url($i) }}">{{ $i }}</a>
+                </li>
+                @endfor
+                <li class="page-item {{ ($paginator->currentPage() == $paginator->lastPage()) ? ' disabled' : '' }}">
+                    <button class="page-link" wire:click="nextPage">Next</button>
+                </li>
+        </ul>
+    </nav>
+    @endif
+</div> --}}
+<div class="d-flex flex-column flex-md-row justify-content-end">
+    <small class="text-muted d-block mb-2 my-md-2 me-1">
+        Showing
+        <span class="font-semibold">{{$paginator->currentPage()}}</span>
+        of
+        <span class="font-semibold">{{$paginator->lastPage()}}</span>
+        Pages
+    </small>
+
+    @if ($paginator->hasPages())
+
+    @php
+        $isDisabled = ($paginator->currentPage() == 1) ? ' disabled' : '';
+    @endphp
+    <nav aria-label="Page Navigation">
+        <ul class="pagination justify-content-start justify-content-lg-end ">
+            <li class="page-item {{$isDisabled}}">
+                <a class="page-link" wire:click="previousPage" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                </a>
+            </li>
+
+            @for ($i = 1; $i <= $paginator->lastPage(); $i++) 
+                @if ($i <=2 || $i>= $paginator->lastPage() - 1 || ($i >=$paginator->currentPage() - 1 && $i <= $paginator->currentPage() + 1))
+                    <li class="page-item {{ ($paginator->currentPage() == $i) ? ' active' : '' }}">
+                        <a class="page-link" href="#" wire:click="gotoPage({{$i}})">{{ $i }}</a>
+                    </li>
+                    @elseif (($i == 3 && $paginator->currentPage() > 4) || ($i == $paginator->lastPage() - 2 &&
+                    $paginator->currentPage() < $paginator->lastPage() - 3)) 
+                    <li class="page-item disabled">
+                        <span class="page-link">...</span>
+                    </li>
+                @endif
+            @endfor
+
+            <li
+                class="page-item {{ ($paginator->currentPage() == $paginator->lastPage()) ? ' disabled' : '' }}">
+                <a class="page-link" wire:click="nextPage" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                </a>
+            </li>
+            &nbsp;
+            &nbsp;
+            <small>
+                <li class="input-group input-group-sm">
+                    <input type="text" id="pageNumberInput" class="form-control form-control-sm text-center" size="1" placeholder="{{$paginator->currentPage()}}">
+                    <button onclick="gotoLivewirePage()" class="btn btn-primary btn-sm">Go</button>
+                </li>
+            </small>
+        </ul>
+        <ul>
+            <small id="errorMessage" class="text-danger" style="display: none;"></small>
+        </ul>
+    
+    </nav>
+    @endif
+</div>
+@push('scripts')
+<script>
+    function gotoLivewirePage() {
             // Get the value from the text field
             let pageNumber = document.getElementById('pageNumberInput').value; 
             if (pageNumber > {{$paginator->lastPage()}}){
@@ -79,5 +96,5 @@
                 @this.call('gotoPage', pageNumber);
             }
         }
-    </script>
+</script>
 @endpush
